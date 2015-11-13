90f9aa94c363b25c41a2682dd663f6e09792d12f
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-11-12 22:45:04 -0800

    updated examples/error_task.py

--- examples/error_task.py
+++ examples/error_task.py
@@ -9,6 +9,4 @@ class ShowError(digdag.BaseTask):
         print "Task failed:"
         print "  * error_message: " + str(self.params["error_message"])
         print "  * error_task_name: " + str(self.params["error_task_name"])
-        print "  * error_retry_at: " + str(self.params["error_retry_at"])
-        print "  * is_subtask_propagation: " + str(self.params["is_subtask_propagation"])
 
