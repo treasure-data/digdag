664996fd3c4562f7cce79bfda92a06ece319c6cc
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-12-14 15:48:17 +0900

    implemented PUT /api/sessions

---
+++ digdag-server/src/main/java/io/digdag/server/RestSessionRequest.java
@@ -0,0 +1,35 @@
+package io.digdag.server;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Optional;
+import org.immutables.value.Value;
+import io.digdag.spi.config.Config;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableRestSessionRequest.class)
+@JsonDeserialize(as = ImmutableRestSessionRequest.class)
+public abstract class RestSessionRequest
+{
+    @JsonProperty("repository")
+    public abstract Optional<String> getRepositoryName();
+
+    @JsonProperty("workflow")
+    public abstract Optional<String> getWorkflowName();
+
+    @JsonProperty("revision")
+    public abstract Optional<String> getRevision();
+
+    @JsonProperty("workflowId")
+    public abstract Optional<Integer> getWorkflowId();
+
+    @JsonProperty("session_name")
+    public abstract Optional<String> getSessionName();
+
+    @JsonProperty("params")
+    public abstract Optional<Config> getSessionParams();
+
+    @JsonProperty("from")
+    public abstract Optional<String> getFromTaskName();
+}
--- digdag-server/src/main/java/io/digdag/server/SessionResource.java
+++ digdag-server/src/main/java/io/digdag/server/SessionResource.java
@@ -1,6 +1,8 @@
 package io.digdag.server;
 
 import java.util.List;
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
@@ -12,10 +14,15 @@ import javax.ws.rs.GET;
 import com.google.inject.Inject;
 import com.google.common.collect.*;
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import io.digdag.core.session.SessionStore;
 import io.digdag.core.session.SessionStoreManager;
 import io.digdag.core.session.StoredSession;
+import io.digdag.core.workflow.*;
+import io.digdag.core.session.*;
 import io.digdag.core.repository.*;
+import io.digdag.spi.config.Config;
+import io.digdag.spi.config.ConfigFactory;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.ArchiveEntry;
@@ -30,20 +37,27 @@ public class SessionResource
     // [*] GET  /api/sessions?repository=<name>&workflow=<name>  # list sessions that belong to a particular workflow
     // [*] GET  /api/sessions/{id}                               # show a session
     // [*] GET  /api/sessions/{id}/tasks                         # list tasks of a session
+    // [*] PUT  /api/sessions                                    # starts a new session (cancel, retry, etc.)
     // [ ] POST /api/sessions/{id}                               # do operations on a session (cancel, retry, etc.)
 
     private final RepositoryStoreManager rm;
     private final SessionStoreManager sm;
+    private final WorkflowExecutor executor;
+    private final ConfigFactory cf;
 
     private int siteId = 0;  // TODO get site id from context
 
     @Inject
     public SessionResource(
             RepositoryStoreManager rm,
-            SessionStoreManager sm)
+            SessionStoreManager sm,
+            WorkflowExecutor executor,
+            ConfigFactory cf)
     {
         this.rm = rm;
         this.sm = sm;
+        this.executor = executor;
+        this.cf = cf;
     }
 
     @GET
@@ -94,5 +108,58 @@ public class SessionResource
             .stream()
             .map(task -> RestTask.of(task))
             .collect(Collectors.toList());
+    }
+
+    @PUT
+    @Path("/api/sessions")
+    public RestSession startTask(RestSessionRequest request)
+        throws ResourceNotFoundException, ResourceConflictException, TaskMatchPattern.MultipleMatchException, TaskMatchPattern.NoMatchException
+    {
+        StoredWorkflowSource wf;
+        SessionRelation rel;
+
+        RepositoryStore rs = rm.getRepositoryStore(siteId);
+        if (request.getWorkflowId().isPresent()) {
+            Preconditions.checkArgument(!request.getRepositoryName().isPresent(), "repository and workflowId can't be set together");
+            Preconditions.checkArgument(!request.getWorkflowName().isPresent(), "workflow and workflowId can't be set together");
+            Preconditions.checkArgument(!request.getRevision().isPresent(), "revision and workflowId can't be set together");
+
+            wf = rs.getWorkflowById(request.getWorkflowId().get());  // validate site id
+            StoredWorkflowSourceWithRepository details = rm.getWorkflowDetailsById(wf.getId());
+            rel = SessionRelation.ofWorkflow(details.getRepository().getId(), details.getRevisionId(), details.getId());
+        }
+        else {
+            Preconditions.checkArgument(request.getRepositoryName().isPresent(), "repository is required if workflowId is not set");
+            Preconditions.checkArgument(request.getWorkflowName().isPresent(), "workflow is required if workflowId is not set");
+            StoredRepository repo = rs.getRepositoryByName(request.getWorkflowName().get());
+            StoredRevision rev;
+            if (request.getRevision().isPresent()) {
+                rev = rs.getRevisionByName(repo.getId(), request.getRevision().get());
+            }
+            else {
+                rev = rs.getLatestActiveRevision(repo.getId());
+            }
+            wf = rs.getWorkflowByName(rev.getId(), request.getWorkflowName().get());
+            rel = SessionRelation.ofWorkflow(repo.getId(), rev.getId(), wf.getId());
+        }
+
+        String sessionName = request.getSessionName().or(UUID.randomUUID().toString());
+
+        Config sessionParams = cf.create();
+        if (request.getSessionParams().isPresent()) {
+            sessionParams.setAll(request.getSessionParams().get());
+        }
+
+        Session session = Session.sessionBuilder()
+            .name(sessionName)
+            .params(sessionParams)
+            .options(SessionOptions.empty())
+            .build();
+
+        StoredSession stored = executor.submitWorkflow(
+                siteId, wf, session, Optional.of(rel),
+                new Date(), request.getFromTaskName().transform(name -> new TaskMatchPattern(name)));
+
+        return RestSession.of(stored);
     }
 }
