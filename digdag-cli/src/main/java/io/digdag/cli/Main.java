10aab74fcae4a8962102e650247ab4f0366e2879
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-03-02 11:34:46 -0800

    v0.3.3

--- digdag-cli/src/main/java/io/digdag/cli/Main.java
+++ digdag-cli/src/main/java/io/digdag/cli/Main.java
@@ -44,10 +44,10 @@ public class Main
         throws Exception
     {
         if (args.length == 1 && args[0].equals("--version")) {
-            System.out.println("0.3.2");
+            System.out.println("0.3.3");
             return;
         }
-        System.err.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z").format(new Date()) + ": Digdag v0.3.2");
+        System.err.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z").format(new Date()) + ": Digdag v0.3.3");
 
         MainOptions mainOpts = new MainOptions();
         JCommander jc = new JCommander(mainOpts);
--- digdag-docs/src/command_reference.rst
+++ digdag-docs/src/command_reference.rst
@@ -198,7 +198,7 @@ Updates the executable binary file to the latest version or specified version. E
 .. code-block:: console
 
     $ digdag selfupdate
-    $ digdag selfupdate 0.3.2
+    $ digdag selfupdate 0.3.3
 
 Server-mode commands
 ----------------------------------
