fe7f6c8cb64e5570f798030e36aada5331657510
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-03-16 10:29:30 -0700

    added missing RemoteDatabaseConfig

---
+++ digdag-core/src/main/java/io/digdag/core/database/RemoteDatabaseConfig.java
@@ -0,0 +1,30 @@
+package io.digdag.core.database;
+
+import java.util.Locale;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.FileSystems;
+import com.google.common.base.Optional;
+import org.immutables.value.Value;
+import io.digdag.client.config.Config;
+import io.digdag.client.config.ConfigException;
+
+@Value.Immutable
+public interface RemoteDatabaseConfig
+{
+    String getUser();
+
+    String getPassword();
+
+    String getHost();
+
+    Optional<Integer> getPort();
+
+    String getDatabase();
+
+    static ImmutableRemoteDatabaseConfig.Builder builder()
+    {
+        return ImmutableRemoteDatabaseConfig.builder();
+    }
+}
