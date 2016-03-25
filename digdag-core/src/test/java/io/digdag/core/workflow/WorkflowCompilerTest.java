bf3d30b46de2b5db8ba2262e329b4a2c03ed9e70
Daniel Norberg <dano@treasure-data.com>
2016-03-25 17:15:32 +0900

    workflow: fail on multiple operators in a task
    https://github.com/treasure-data/digdag/issues/21

---
+++ digdag-core/src/test/java/io/digdag/core/workflow/WorkflowCompilerTest.java
@@ -0,0 +1,63 @@
+package io.digdag.core.workflow;
+
+import com.google.common.base.Throwables;
+import com.google.common.io.Resources;
+import io.digdag.client.config.Config;
+import io.digdag.client.config.ConfigException;
+import io.digdag.client.config.ConfigFactory;
+import io.digdag.core.DigdagEmbed;
+import io.digdag.core.config.YamlConfigLoader;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+
+import static io.digdag.core.workflow.WorkflowTestingUtils.setupEmbed;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class WorkflowCompilerTest
+{
+
+    @Rule public ExpectedException exception = ExpectedException.none();
+
+    private DigdagEmbed embed;
+
+    private WorkflowCompiler compiler;
+
+    @Before
+    public void setUp()
+            throws Exception
+    {
+        embed = setupEmbed();
+        compiler = new WorkflowCompiler();
+    }
+
+    private Config loadYamlResource(String name)
+    {
+        try {
+            String content = Resources.toString(getClass().getResource(name), UTF_8);
+            return embed.getInjector().getInstance(YamlConfigLoader.class)
+                    .loadString(content)
+                    .toConfig(embed.getInjector().getInstance(ConfigFactory.class));
+        }
+        catch (IOException ex) {
+            throw Throwables.propagate(ex);
+        }
+    }
+
+    @Test
+    public void verifySingleOperatorPasses() {
+        Config config = loadYamlResource("/digdag/workflow/cases/single_operator.yml");
+        compiler.compile("+foo", config);
+    }
+
+    @Test
+    public void verifyMultipleOperatorsFail()
+    {
+        Config config = loadYamlResource("/digdag/workflow/cases/multiple_operators.yml");
+        exception.expect(ConfigException.class);
+        compiler.compile("+foo", config);
+    }
+}
