309765a5eb34e67a67bfe30566243098c2f86c5d
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-04-06 14:13:48 -0700

    Simplified TaskTree

--- digdag-core/src/main/java/io/digdag/core/workflow/TaskTree.java
+++ digdag-core/src/main/java/io/digdag/core/workflow/TaskTree.java
@@ -1,5 +1,6 @@
 package io.digdag.core.workflow;
 
+import java.util.Objects;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -12,162 +13,59 @@ import io.digdag.core.session.TaskRelation;
 
 public class TaskTree
 {
-    // TODO This class includes unnecessary complexity. Node class is unnecessary.
-    //      if walk methods lookup Map<Long, TaskRelation> when necessary
-
-    public static class Node
-    {
-        private final TaskRelation relation;
-        private Optional<Node> parent;
-        private List<Node> children = new ArrayList<>();
-        private List<Node> upstreams = new ArrayList<>();
-
-        private Node(TaskRelation relation)
-        {
-            this.relation = relation;
-        }
-
-        public long getId()
-        {
-            return relation.getId();
-        }
-
-        public Optional<Node> getParent()
-        {
-            return parent;
-        }
-
-        public List<Node> getChildren()
-        {
-            return children;
-        }
-
-        public List<Node> getUpstreams()
-        {
-            return upstreams;
-        }
-
-        private TaskRelation getRelation()
-        {
-            return relation;
-        }
-
-        private void setParent(Optional<Node> parent)
-        {
-            this.parent = parent;
-        }
-
-        private void addChild(Node child)
-        {
-            this.children.add(child);
-        }
-
-        private void addUpstream(Node upstream)
-        {
-            this.upstreams.add(upstream);
-        }
-
-        public <T> T walkParentsRecursively(T value, Walker<T> walker)
-        {
-            if (!parent.isPresent()) {
-                return value;
-            }
-            else {
-                value = walker.walk(value, parent.get());
-                return parent.get().walkParentsRecursively(value, walker);
-            }
-        }
-
-        public <T> T walkChildrenRecursively(T value, Walker<T> walker)
-        {
-            for (Node child : children) {
-                value = walker.walk(value, child);
-                value = child.walkChildrenRecursively(value, walker);
-            }
-            return value;
-        }
-
-        public <T> T walkUpstreamSiblings(T value, Walker<T> walker)
-        {
-            return walkUpstreamSiblings(value, walker, new HashSet<>());
-        }
-
-        private <T> T walkUpstreamSiblings(T value, Walker<T> walker, Set<Long> walkedSet)
-        {
-            for (Node up : upstreams) {
-                if (!walkedSet.contains(up.getId())) {
-                    walkedSet.add(up.getId());
-                    value = walker.walk(value, up);
-                    value = up.walkUpstreamSiblings(value, walker, walkedSet);
-                }
-            }
-            return value;
-        }
-    }
-
     public interface Walker <T>
     {
-        T walk(T value, Node node);
+        T walk(T value, TaskRelation node);
     }
 
-    private static Map<Long, Node> buildTree(List<TaskRelation> rels)
-    {
-        Map<Long, Node> map = new HashMap<>();
-        for (TaskRelation rel : rels) {
-            map.put(rel.getId(), new Node(rel));
-        }
-
-        // set parent and children
-        for (Node node : map.values()) {
-            Optional<Long> parentId = node.getRelation().getParentId();
-            if (parentId.isPresent()) {
-                Node parent = map.get(parentId.get());
-                node.setParent(Optional.of(parent));
-                parent.addChild(node);
-            }
-            else {
-                node.setParent(Optional.absent());
-            }
-        }
-
-        // set upstreams
-        for (Node node : map.values()) {
-            for (long id : node.getRelation().getUpstreams()) {
-                Node upstream = map.get(id);
-                node.addUpstream(upstream);
-            }
-        }
-
-        return map;
-    }
-
-    private final Map<Long, Node> map;
+    private final Map<Long, TaskRelation> map;
 
     public TaskTree(List<TaskRelation> rels)
     {
-        this.map = buildTree(rels);
+        ImmutableMap.Builder<Long, TaskRelation> builder = ImmutableMap.builder();
+        for (TaskRelation rel : rels) {
+            builder.put(rel.getId(), rel);
+        }
+        this.map = builder.build();
     }
 
     public long getRootTaskId()
     {
-        for (Node node : map.values()) {
-            if (!node.getParent().isPresent()) {
-                return node.getId();
+        for (TaskRelation rel : map.values()) {
+            if (!rel.getParentId().isPresent()) {
+                return rel.getId();
             }
         }
         throw new IllegalStateException("Root task doesn't exist in an attempt: "+map.values());
     }
 
-    public Node getNode(long id)
+    private TaskRelation get(long id)
     {
-        return Preconditions.checkNotNull(map.get(id));
+        return Objects.requireNonNull(map.get(id));
+    }
+
+    private Optional<TaskRelation> getParent(long id)
+    {
+        return get(id).getParentId().transform(it -> get(it));
     }
 
     public List<Long> getRecursiveParentIdList(long id)
     {
-        return getNode(id).walkParentsRecursively(
+        return walkParentsRecursively(id,
                 ImmutableList.<Long>builder(),
                 (builder, parent) -> builder.add(parent.getId())).build();
+    }
+
+    private <T> T walkParentsRecursively(long id, T value, Walker<T> walker)
+    {
+        Optional<TaskRelation> parent = getParent(id);
+        if (!parent.isPresent()) {
+            return value;
+        }
+        else {
+            value = walker.walk(value, parent.get());
+            return walkParentsRecursively(parent.get().getId(), value, walker);
+        }
     }
 
     public List<Long> getRecursiveParentsUpstreamChildrenIdList(long id)
@@ -179,19 +77,49 @@ public class TaskTree
             .stream()
             .forEach(parentId -> {
                 builder.add(parentId);
-                getNode(parentId).walkUpstreamSiblings(
+                walkUpstreamSiblings(id,
                         builder,
                         (sameBuilder1, sib) -> {
                             sameBuilder1.add(sib.getId());
-                            return sib.walkChildrenRecursively(sameBuilder1, (sameBuilder2, child) -> sameBuilder2.add(child.getId()));
+                            return walkChildrenRecursively(sib.getId(), sameBuilder1, (sameBuilder2, child) -> sameBuilder2.add(child.getId()));
                         });
             });
         return builder.build();
     }
 
+    public <T> T walkChildrenRecursively(long id, T value, Walker<T> walker)
+    {
+        for (TaskRelation rel : map.values()) {
+            if (rel.getParentId().isPresent() && rel.getParentId().get() == id) {
+                TaskRelation child = rel;
+                value = walker.walk(value, child);
+                value = walkChildrenRecursively(child.getId(), value, walker);
+            }
+        }
+        return value;
+    }
+
+    private <T> T walkUpstreamSiblings(long id, T value, Walker<T> walker)
+    {
+        return walkUpstreamSiblings(id, value, walker, new HashSet<>());
+    }
+
+    private <T> T walkUpstreamSiblings(long id, T value, Walker<T> walker, Set<Long> walkedSet)
+    {
+        for (long upId : get(id).getUpstreams()) {
+            TaskRelation up = get(upId);
+            if (!walkedSet.contains(up.getId())) {
+                walkedSet.add(up.getId());
+                value = walker.walk(value, up);
+                value = walkUpstreamSiblings(up.getId(), value, walker, walkedSet);
+            }
+        }
+        return value;
+    }
+
     public List<Long> getRecursiveChildrenIdList(long id)
     {
-        return getNode(id).walkChildrenRecursively(
+        return walkChildrenRecursively(id,
                 ImmutableList.<Long>builder(),
                 (builder, child) -> builder.add(child.getId())).build();
     }
