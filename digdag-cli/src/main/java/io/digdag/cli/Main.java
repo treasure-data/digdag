9f094af911e7a228074889983d0ab2895fd7abec
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-04-08 17:15:53 -0700

    v.5.9

--- digdag-cli/src/main/java/io/digdag/cli/Main.java
+++ digdag-cli/src/main/java/io/digdag/cli/Main.java
@@ -106,10 +106,10 @@ public class Main
         throws Exception
     {
         if (args.length == 1 && args[0].equals("--version")) {
-            System.out.println("0.5.8");
+            System.out.println("0.5.9");
             return;
         }
-        System.err.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z").format(new Date()) + ": Digdag v0.5.8");
+        System.err.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z").format(new Date()) + ": Digdag v0.5.9");
 
         MainOptions mainOpts = new MainOptions();
         JCommander jc = new JCommander(mainOpts);
---
+++ digdag-docs/src/releases/release-0.5.9.rst
@@ -0,0 +1,20 @@
+Release 0.5.9
+==================================
+
+Operator changes
+------------------
+
+* Added ``call>`` operator.
+
+* ``for_each>`` operator ecodes task name task name using URL encoding. This fixes "invalid task name" error when a value is array or map.
+
+
+General changes
+------------------
+
+* Added ``_workdir`` option. This will be used internally in future release.
+
+
+Release Date
+------------------
+2016-04-08
