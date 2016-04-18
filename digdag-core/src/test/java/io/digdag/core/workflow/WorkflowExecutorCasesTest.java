2c9c57175fd8be5f780e7404326bf18164531778
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-04-18 14:40:52 -0700

    added test case for a validation of unknown toplevel keys

--- digdag-core/src/test/java/io/digdag/core/workflow/WorkflowExecutorCasesTest.java
+++ digdag-core/src/test/java/io/digdag/core/workflow/WorkflowExecutorCasesTest.java
@@ -6,7 +6,6 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.util.concurrent.atomic.AtomicReference;
 import org.skife.jdbi.v2.IDBI;
-import org.junit.*;
 import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
 import com.google.common.collect.*;
@@ -20,21 +19,32 @@ import io.digdag.core.workflow.*;
 import io.digdag.core.config.YamlConfigLoader;
 import io.digdag.spi.ScheduleTime;
 import io.digdag.client.config.Config;
+import io.digdag.client.config.ConfigException;
 import io.digdag.client.config.ConfigFactory;
 import io.digdag.core.DigdagEmbed;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static io.digdag.core.workflow.WorkflowTestingUtils.*;
 import static org.junit.Assert.*;
 
 public class WorkflowExecutorCasesTest
 {
+    @Rule public ExpectedException exception = ExpectedException.none();
+
     private DigdagEmbed embed;
+
+    private ConfigFactory cf;
 
     @Before
     public void setUp()
         throws Exception
     {
         embed = setupEmbed();
+        cf = embed.getInjector().getInstance(ConfigFactory.class);
     }
 
     @After
@@ -51,13 +61,24 @@ public class WorkflowExecutorCasesTest
         runWorkflow("basic", loadYamlResource("/digdag/workflow/cases/basic.yml"));
     }
 
+    @Test
+    public void validateUnknownTopLevelKeys()
+        throws Exception
+    {
+        exception.expect(ConfigException.class);
+        runWorkflow("unknown_schedule", cf.create()
+                .set("invalid_schedule", cf.create().set("daily>", "00:00:00"))
+                .set("+step1", cf.create().set("sh>", "echo ok"))
+                );
+    }
+
     private Config loadYamlResource(String name)
     {
         try {
             String content = Resources.toString(getClass().getResource(name), UTF_8);
             return embed.getInjector().getInstance(YamlConfigLoader.class)
                 .loadString(content)
-                .toConfig(embed.getInjector().getInstance(ConfigFactory.class));
+                .toConfig(cf);
         }
         catch (IOException ex) {
             throw Throwables.propagate(ex);
