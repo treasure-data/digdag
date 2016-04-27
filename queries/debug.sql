f96a5a866a31fec2f74f7c7f928f8d5614cb66fe
Daniel Norberg <dano@treasure-data.com>
2016-04-27 15:00:30 +0900

    try out more features

---
+++ queries/debug.sql
@@ -0,0 +1,1 @@
+SELECT TD_TIME_FORMAT(TD_SCHEDULED_TIME(), 'yyyy-MM-dd HH:mm:ss z', 'UTC');
--- queries/www_access.sql
+++ queries/www_access.sql
@@ -1,1 +1,6 @@
-SELECT count(*) FROM sample_datasets.www_access LIMIT 10;
+SELECT count(*)
+FROM sample_datasets.www_access
+WHERE TD_TIME_RANGE(time,
+    TD_TIME_ADD(TD_DATE_TRUNC('day', TD_SCHEDULED_TIME()), '-${from_days_ago}d'),
+    TD_TIME_ADD(TD_DATE_TRUNC('day', TD_SCHEDULED_TIME()), '-${to_days_ago}d')
+);
