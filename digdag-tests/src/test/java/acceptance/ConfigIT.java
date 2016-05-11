2b13fec3e02785402928fe1ff3dff10b0d363777
Daniel Norberg <dano@treasure-data.com>
2016-05-11 17:45:28 +0900

    verify command line param overrides config param

--- digdag-tests/src/test/java/acceptance/ConfigIT.java
+++ digdag-tests/src/test/java/acceptance/ConfigIT.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import static acceptance.TestUtils.copyResource;
 import static acceptance.TestUtils.main;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -25,7 +26,8 @@ public class ConfigIT
     }
 
     @Test
-    public void propertyByFile() throws Exception
+    public void propertyByFile()
+            throws Exception
     {
         copyResource("acceptance/params.yml", root().resolve("params.yml"));
         TestUtils.fakeHome(root().resolve("home").toString(), () -> {
@@ -33,5 +35,20 @@ public class ConfigIT
             main("run", "-o", root().toString(), "-f", root().resolve("params.yml").toString());
         });
         assertThat(Files.readAllBytes(root().resolve("foo.out")), is("secret\n".getBytes(UTF_8)));
+    }
+
+    @Test
+    public void verifyThatCommandLineParamsOverridesConfigFileParams()
+            throws Exception
+    {
+        copyResource("acceptance/params.yml", root().resolve("params.yml"));
+        TestUtils.fakeHome(root().resolve("home").toString(), () -> {
+            Files.write(root().resolve("home").resolve(".digdag").resolve("config"), "params.mysql.password=secret".getBytes(UTF_8));
+            main("run",
+                    "-o", root().toString(),
+                    "-f", root().resolve("params.yml").toString(),
+                    "-p", "mysql.password=override");
+        });
+        assertThat(Files.readAllLines(root().resolve("foo.out")), contains("override"));
     }
 }
