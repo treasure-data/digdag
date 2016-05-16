7ce283b55b1b57a17691d7579bc6fd545678123c
Daniel Norberg <dano@treasure-data.com>
2016-05-16 14:26:12 +0900

    add server-side workflow error mail test

---
+++ digdag-tests/src/test/java/acceptance/ErrorServerMailIT.java
@@ -0,0 +1,140 @@
+package acceptance;
+
+import com.google.common.base.Joiner;
+import io.digdag.client.DigdagClient;
+import io.digdag.client.api.RestSessionAttempt;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static acceptance.TestUtils.copyResource;
+import static acceptance.TestUtils.getStartAttemptId;
+import static acceptance.TestUtils.main;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+public class ErrorServerMailIT
+{
+    private static final String SENDER = "alert@digdag.io";
+    private static final String RECEIVER = "test@digdag.io";
+    private static final String LOCAL_SESSION_TIME = "2016-01-02 03:04:05";
+    private static final String SESSION_TIME_ISO = "2016-01-02T03:04:05+00:00";
+    private static final String HOSTNAME = "127.0.0.1";
+
+    private final int port = TestUtils.findFreePort();
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Rule
+    public TemporaryDigdagServer server = TemporaryDigdagServer.builder()
+            .configuration(Joiner.on("\n").join(asList(
+                    "config.mail.host=" + HOSTNAME,
+                    "config.mail.port=" + port,
+                    "config.mail.from=" + SENDER,
+                    "config.mail.username=mail-user",
+                    "config.mail.password=mail-pass",
+                    "config.mail.tls=false"
+            )))
+            .build();
+
+    private Path config;
+    private Path projectDir;
+
+    private Wiser mailServer;
+    private Path projectFile;
+
+    @Before
+    public void setUp()
+            throws Exception
+    {
+        config = folder.newFile().toPath();
+
+        projectDir = folder.getRoot().toPath().resolve("error");
+        Files.createDirectory(projectDir);
+
+        projectFile = projectDir.resolve("digdag.yml");
+
+        copyResource("acceptance/error_server_mail/digdag.yml", projectFile);
+        copyResource("acceptance/error_server_mail/error.yml", projectDir.resolve("error.yml"));
+        copyResource("acceptance/error_server_mail/fail.sql", projectDir.resolve("fail.sql"));
+        copyResource("acceptance/error_server_mail/alert.txt", projectDir.resolve("alert.txt"));
+
+        mailServer = new Wiser();
+        mailServer.setHostname(HOSTNAME);
+        mailServer.setPort(port);
+        mailServer.start();
+    }
+
+    @After
+    public void tearDown()
+            throws Exception
+    {
+        if (mailServer != null) {
+            mailServer.stop();
+        }
+    }
+
+    @Test
+    public void verifyMailIsSentWhenWorkflowFails()
+            throws Exception
+    {
+        DigdagClient client = DigdagClient.builder()
+                .host(server.host())
+                .port(server.port())
+                .build();
+
+        // Push the project
+        CommandStatus pushStatus = main("push",
+                "error",
+                "-c", config.toString(),
+                "-f", projectFile.toString(),
+                "-e", server.endpoint(),
+                "-r", "4711");
+        assertThat(pushStatus.code(), is(0));
+
+        // Start the workflow
+        CommandStatus startStatus = main("start",
+                "-c", config.toString(),
+                "-e", server.endpoint(),
+                "error", "error",
+                "--session", LOCAL_SESSION_TIME);
+        assertThat(startStatus.code(), is(0));
+        long attemptId = TestUtils.getStartAttemptId(startStatus);
+
+        // Wait for the attempt to fail
+        RestSessionAttempt attempt = null;
+        for (int i = 0; i < 30; i++) {
+            attempt = client.getSessionAttempt(attemptId);
+            if (attempt.getDone()) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        assertThat(attempt.getSuccess(), is(false));
+
+        // Wait for mail to be delivered
+        for (int i = 0; i < 30; i++) {
+            if (mailServer.getMessages().size() > 0) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        // Verify that the mail was correctly delivered
+        assertThat(mailServer.getMessages().size(), is(1));
+        WiserMessage message = mailServer.getMessages().get(0);
+        assertThat(message.getEnvelopeSender(), is(SENDER));
+        assertThat(message.getEnvelopeReceiver(), is(RECEIVER));
+        assertThat(message.getMimeMessage().getContent(), is("alert " + SESSION_TIME_ISO + "\r\n"));
+    }
+}
