7497728a8d97fef03a23cc353f23930e18272823
Daniel Norberg <dano@treasure-data.com>
2016-05-18 12:15:58 +0900

    start 0.7.2

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -58,7 +58,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ digdag selfupdate");
-        err.println("    $ digdag selfupdate 0.7.1");
+        err.println("    $ digdag selfupdate 0.7.2-SNAPSHOT");
         err.println("");
         return systemExit(error);
     }
