30855b6d43c3d3928ef59a4e5b23dcc969e5092c
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-05-30 17:46:09 +0900

    added OperatorModule.addStandardOperatorFactory
    This is useful when application integrators can override it to disable
    standard operators using blacklists.

--- digdag-standards/src/main/java/io/digdag/standards/StandardsExtension.java
+++ digdag-standards/src/main/java/io/digdag/standards/StandardsExtension.java
@@ -1,37 +1,12 @@
 package io.digdag.standards;
 
 import java.util.List;
-import com.google.inject.Binder;
-import com.google.inject.Module;
-import com.google.inject.Scopes;
-import com.google.inject.multibindings.Multibinder;
 import com.google.common.collect.ImmutableList;
+import com.google.inject.Module;
 import io.digdag.core.Extension;
-import io.digdag.spi.SchedulerFactory;
-import io.digdag.spi.OperatorFactory;
-import io.digdag.spi.CommandExecutor;
-import io.digdag.standards.operator.EchoOperatorFactory;
-import io.digdag.standards.operator.NopOperatorFactory;
-import io.digdag.standards.scheduler.CronSchedulerFactory;
-import io.digdag.standards.scheduler.MonthlySchedulerFactory;
-import io.digdag.standards.scheduler.WeeklySchedulerFactory;
-import io.digdag.standards.scheduler.DailySchedulerFactory;
-import io.digdag.standards.scheduler.HourlySchedulerFactory;
-import io.digdag.standards.scheduler.MinutesIntervalSchedulerFactory;
-import io.digdag.standards.operator.PyOperatorFactory;
-import io.digdag.standards.operator.RbOperatorFactory;
-import io.digdag.standards.operator.ShOperatorFactory;
-import io.digdag.standards.operator.MailOperatorFactory;
-import io.digdag.standards.operator.LoopOperatorFactory;
-import io.digdag.standards.operator.ForEachOperatorFactory;
-import io.digdag.standards.operator.EmbulkOperatorFactory;
-import io.digdag.standards.operator.td.TdOperatorFactory;
-import io.digdag.standards.operator.td.TdRunOperatorFactory;
-import io.digdag.standards.operator.td.TdLoadOperatorFactory;
-import io.digdag.standards.operator.td.TdDdlOperatorFactory;
-import io.digdag.standards.operator.td.TdTableExportOperatorFactory;
-import io.digdag.standards.command.SimpleCommandExecutor;
-import io.digdag.standards.command.DockerCommandExecutor;
+import io.digdag.standards.operator.OperatorModule;
+import io.digdag.standards.scheduler.SchedulerModule;
+import io.digdag.standards.command.CommandExecutorModule;
 
 public class StandardsExtension
         implements Extension
@@ -39,41 +14,10 @@ public class StandardsExtension
     @Override
     public List<Module> getModules()
     {
-        return ImmutableList.of(new StandardsModule());
-    }
-
-    private static class StandardsModule
-        implements Module
-    {
-        @Override
-        public void configure(Binder binder)
-        {
-            //binder.bind(CommandExecutor.class).to(SimpleCommandExecutor.class).in(Scopes.SINGLETON);
-            binder.bind(CommandExecutor.class).to(DockerCommandExecutor.class).in(Scopes.SINGLETON);
-            binder.bind(SimpleCommandExecutor.class).in(Scopes.SINGLETON);
-
-            Multibinder<OperatorFactory> operatorFactoryBinder = Multibinder.newSetBinder(binder, OperatorFactory.class);
-            operatorFactoryBinder.addBinding().to(PyOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(RbOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(ShOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(MailOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(LoopOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(ForEachOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(EmbulkOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(TdOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(TdRunOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(TdLoadOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(TdDdlOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(TdTableExportOperatorFactory.class).in(Scopes.SINGLETON);
-            operatorFactoryBinder.addBinding().to(EchoOperatorFactory.class).in(Scopes.SINGLETON);
-
-            Multibinder<SchedulerFactory> schedulerBinder = Multibinder.newSetBinder(binder, SchedulerFactory.class);
-            schedulerBinder.addBinding().to(CronSchedulerFactory.class).in(Scopes.SINGLETON);
-            schedulerBinder.addBinding().to(MonthlySchedulerFactory.class).in(Scopes.SINGLETON);
-            schedulerBinder.addBinding().to(WeeklySchedulerFactory.class).in(Scopes.SINGLETON);
-            schedulerBinder.addBinding().to(DailySchedulerFactory.class).in(Scopes.SINGLETON);
-            schedulerBinder.addBinding().to(HourlySchedulerFactory.class).in(Scopes.SINGLETON);
-            schedulerBinder.addBinding().to(MinutesIntervalSchedulerFactory.class).in(Scopes.SINGLETON);
-        }
+        return ImmutableList.of(
+                new SchedulerModule(),
+                new CommandExecutorModule(),
+                new OperatorModule()
+                );
     }
 }
