0fcc4a7542d8a2238dc17b929d51d3f2b0b546b8
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-05-30 17:57:09 +0900

    Add if> and fail> experimental operators
    These operators are useful to validate results of a task at _check
    directive so that workflow fails when validation doesn't pass.

---
+++ digdag-standards/src/main/java/io/digdag/standards/operator/IfOperatorFactory.java
@@ -0,0 +1,57 @@
+package io.digdag.standards.operator;
+
+import java.nio.file.Path;
+import com.google.inject.Inject;
+import io.digdag.spi.TaskRequest;
+import io.digdag.spi.TaskResult;
+import io.digdag.spi.Operator;
+import io.digdag.spi.OperatorFactory;
+import io.digdag.client.config.Config;
+
+public class IfOperatorFactory
+        implements OperatorFactory
+{
+    @Inject
+    public IfOperatorFactory()
+    { }
+
+    public String getType()
+    {
+        return "if";
+    }
+
+    @Override
+    public Operator newTaskExecutor(Path workspacePath, TaskRequest request)
+    {
+        return new IfOperator(workspacePath, request);
+    }
+
+    private static class IfOperator
+            extends BaseOperator
+    {
+        public IfOperator(Path workspacePath, TaskRequest request)
+        {
+            super(workspacePath, request);
+        }
+
+        @Override
+        public TaskResult runTask()
+        {
+            Config params = request.getConfig();
+
+            Config doConfig = request.getConfig().getNested("_do");
+
+            boolean condition = params.get("_command", boolean.class);
+
+            if (condition) {
+                return TaskResult.defaultBuilder(request)
+                    .subtaskConfig(doConfig)
+                    .build();
+            }
+            else {
+                return TaskResult.defaultBuilder(request)
+                    .build();
+            }
+        }
+    }
+}
