f88599363e8af75b37c7c2a87ab4f96da2fdf9d9
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-06-03 17:36:14 +0900

    fixed comments

--- digdag-core/src/main/java/io/digdag/core/database/DataSourceProvider.java
+++ digdag-core/src/main/java/io/digdag/core/database/DataSourceProvider.java
@@ -49,15 +49,15 @@ public class DataSourceProvider
     {
         String url = DatabaseConfig.buildJdbcUrl(config);
 
-        // H2 closes the database when all of the connections are closed by default. When database
+        // By default, H2 closes database when all of the connections are closed. When database
         // is closed, data is gone with in-memory mode. It's unexpected. However, if here disables
-        // that behavior using DB_CLOSE_DELAY=-1 option, there're no methods to close the database
-        // explicitly. Only method to close is to not disable shutdown hook of H2 database
-        // (DB_CLOSE_ON_EXIT=TRUE). But this is also causes unexpected behavior if PreDestroy is
-        // triggered at shutdown hook. Therefore, here needs to depend on injector to resolve
-        // dependencies so that database is closed after calling all other PreDestroy methods that
-        // depend on DataSourceProvider.
-        // To solve this issue, here holds one Connection without using DB_CLOSE_DELAY=-1 option.
+        // that such behavior using DB_CLOSE_DELAY=-1 option, there're no methods to close the
+        // database explicitly. Only way to close is to not disable shutdown hook of H2 database
+        // (DB_CLOSE_ON_EXIT=TRUE). But this also causes unexpected behavior when PreDestroy is
+        // triggered at shutdown hook. Therefore, here needs to rely on injector to take care of
+        // dependencies so that the database is closed after calling all other PreDestroy methods
+        // that depend on this DataSourceProvider.
+        // To solve this issue, here holds one Connection until PreDestroy.
         JdbcDataSource ds = new JdbcDataSource();
         ds.setUrl(url + ";DB_CLOSE_ON_EXIT=FALSE");
 
