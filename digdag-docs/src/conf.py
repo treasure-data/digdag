4d0b96ea67a78d4cec43f3130a199d33a0fa5193
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-06-13 13:07:59 -0700

    docs: support markdown

--- digdag-docs/src/conf.py
+++ digdag-docs/src/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import shlex
+from recommonmark.parser import CommonMarkParser
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -39,7 +40,11 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+
+source_parsers = {
+  '.md': CommonMarkParser,
+}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
