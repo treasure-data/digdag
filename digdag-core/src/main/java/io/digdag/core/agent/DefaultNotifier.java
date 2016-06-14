92521bb3be3a30d72ec54fbb9a63f08fe67d0e05
Daniel Norberg <dano@treasure-data.com>
2016-06-14 10:36:33 +0900

    remove ms suffix

--- digdag-core/src/main/java/io/digdag/core/agent/DefaultNotifier.java
+++ digdag-core/src/main/java/io/digdag/core/agent/DefaultNotifier.java
@@ -5,21 +5,13 @@ import com.google.common.base.Throwables;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import io.digdag.client.config.Config;
-import io.digdag.core.repository.ProjectStoreManager;
-import io.digdag.core.repository.ResourceNotFoundException;
-import io.digdag.core.repository.StoredProject;
 import io.digdag.spi.Notification;
 import io.digdag.spi.NotificationException;
 import io.digdag.spi.NotificationSender;
 import io.digdag.spi.Notifier;
-import io.digdag.spi.TaskRequest;
 import io.digdag.util.RetryExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
 
 import static io.digdag.util.RetryExecutor.retryExecutor;
 
@@ -32,19 +24,19 @@ public class DefaultNotifier
     private static final String NOTIFICATION_TYPE_SHELL = "shell";
 
     private static final String NOTIFICATION_RETRIES = "notification.retries";
-    private static final String NOTIFICATION_MIN_RETRY_WAIT_MS = "notification.min_retry_wait_ms";
-    private static final String NOTIFICATION_MAX_RETRY_WAIT_MS = "notification.max_retry_wait_ms";
+    private static final String NOTIFICATION_MIN_RETRY_WAIT = "notification.min_retry_wait";
+    private static final String NOTIFICATION_MAX_RETRY_WAIT = "notification.max_retry_wait";
     private static final int NOTIFICATION_RETRIES_DEFAULT = 10;
-    private static final int NOTIFICATION_MIN_RETRY_WAIT_DEFAULT_MS = 1000;
-    private static final int NOTIFICATION_MAX_RETRY_WAIT_DEFAULT_MS = 30000;
+    private static final int NOTIFICATION_MIN_RETRY_WAIT_DEFAULT = 1000;
+    private static final int NOTIFICATION_MAX_RETRY_WAIT_DEFAULT = 30000;
 
     private static Logger logger = LoggerFactory.getLogger(DefaultNotifier.class);
 
     private Injector injector;
     private final NotificationSender sender;
     private final int retries;
-    private final int minRetryWaitMs;
-    private final int maxRetryWaitMs;
+    private final int minRetryWait;
+    private final int maxRetryWait;
 
     @Inject
     public DefaultNotifier(Config systemConfig, Injector injector)
@@ -53,8 +45,8 @@ public class DefaultNotifier
         Optional<String> type = systemConfig.getOptional(NOTIFICATION_TYPE, String.class);
         this.sender = type.isPresent() ? sender(type.get()) : null;
         this.retries = systemConfig.get(NOTIFICATION_RETRIES, int.class, NOTIFICATION_RETRIES_DEFAULT);
-        this.minRetryWaitMs = systemConfig.get(NOTIFICATION_MIN_RETRY_WAIT_MS, int.class, NOTIFICATION_MIN_RETRY_WAIT_DEFAULT_MS);
-        this.maxRetryWaitMs = systemConfig.get(NOTIFICATION_MAX_RETRY_WAIT_MS, int.class, NOTIFICATION_MAX_RETRY_WAIT_DEFAULT_MS);
+        this.minRetryWait = systemConfig.get(NOTIFICATION_MIN_RETRY_WAIT, int.class, NOTIFICATION_MIN_RETRY_WAIT_DEFAULT);
+        this.maxRetryWait = systemConfig.get(NOTIFICATION_MAX_RETRY_WAIT, int.class, NOTIFICATION_MAX_RETRY_WAIT_DEFAULT);
     }
 
     private NotificationSender sender(String type)
@@ -84,8 +76,8 @@ public class DefaultNotifier
 
         RetryExecutor retryExecutor = retryExecutor()
                 .retryIf(exception -> true)
-                .withInitialRetryWait(minRetryWaitMs)
-                .withMaxRetryWait(maxRetryWaitMs)
+                .withInitialRetryWait(minRetryWait)
+                .withMaxRetryWait(maxRetryWait)
                 .onRetry((exception, retryCount, retryLimit, retryWait) -> logger.warn("Sending notification failed: retry {} of {}", retryCount, retryLimit, exception))
                 .withRetryLimit(retries);
 
