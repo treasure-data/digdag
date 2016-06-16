5f601d61db7eb83ff4bd18e430912830962a0207
Daniel Norberg <daniel.norberg@gmail.com>
2016-06-16 12:39:43 +0900

    ci: fix deployment script

--- ci/run_deployment.sh
+++ ci/run_deployment.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -xe
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 docker run \
 -w /digdag \
 -v `pwd`/:/digdag \
 -v ~/.gradle:/root/.gradle \
 -e GITHUB_TOKEN \
 digdag-build \
-"${BASEDIR}/push_gh_pages.sh"
+ci/push_gh_pages.sh
