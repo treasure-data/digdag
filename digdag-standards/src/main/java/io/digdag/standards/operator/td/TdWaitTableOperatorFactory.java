4f5415e08453660fed4a56b07e411f10dabf2d6c
Daniel Norberg <dano@treasure-data.com>
2016-06-16 16:26:50 +0900

    cleanup

--- digdag-standards/src/main/java/io/digdag/standards/operator/td/TdWaitTableOperatorFactory.java
+++ digdag-standards/src/main/java/io/digdag/standards/operator/td/TdWaitTableOperatorFactory.java
@@ -13,7 +13,6 @@ import io.digdag.spi.OperatorFactory;
 import io.digdag.spi.TaskExecutionException;
 import io.digdag.spi.TaskRequest;
 import io.digdag.spi.TaskResult;
-import io.digdag.spi.TemplateEngine;
 import io.digdag.util.BaseOperator;
 import org.msgpack.core.MessageTypeCastException;
 import org.msgpack.value.ArrayValue;
@@ -25,8 +24,6 @@ import org.slf4j.LoggerFactory;
 import java.math.BigInteger;
 import java.nio.file.Path;
 
-import static io.digdag.standards.operator.td.TdOperatorFactory.joinJob;
-
 public class TdWaitTableOperatorFactory
         extends AbstractWaitOperatorFactory
         implements OperatorFactory
@@ -35,13 +32,10 @@ public class TdWaitTableOperatorFactory
 
     private static final String JOB_ID = "jobId";
 
-    private final TemplateEngine templateEngine;
-
     @Inject
-    public TdWaitTableOperatorFactory(TemplateEngine templateEngine, Config systemConfig)
+    public TdWaitTableOperatorFactory(Config systemConfig)
     {
         super(systemConfig);
-        this.templateEngine = templateEngine;
     }
 
     public String getType()
@@ -58,7 +52,7 @@ public class TdWaitTableOperatorFactory
     private class TdWaitTableOperator
             extends BaseOperator
     {
-        public TdWaitTableOperator(Path workspacePath, TaskRequest request)
+        private TdWaitTableOperator(Path workspacePath, TaskRequest request)
         {
             super(workspacePath, request);
         }
@@ -70,12 +64,9 @@ public class TdWaitTableOperatorFactory
                     request.getConfig().getNestedOrGetEmpty("td"));
 
             int pollInterval = getPollInterval(params);
-
+            int tableExistencePollInterval = Integer.min(pollInterval, TABLE_EXISTENCE_API_POLL_INTERVAL);
             TableParam table = params.get("_command", TableParam.class);
-
             int rows = params.get("rows", int.class, 0);
-
-
             String engine = params.get("engine", String.class, "presto");
 
             Config state = request.getLastStateParams().deepCopy();
@@ -87,7 +78,7 @@ public class TdWaitTableOperatorFactory
                 // Check if table exists using rest api (if the query job has not yet been started)
                 if (!existingJobId.isPresent()) {
                     if (!op.tableExists(table.getTable())) {
-                        throw TaskExecutionException.ofNextPolling(TABLE_EXISTENCE_API_POLL_INTERVAL, ConfigElement.copyOf(state));
+                        throw TaskExecutionException.ofNextPolling(tableExistencePollInterval, ConfigElement.copyOf(state));
                     }
 
                     // If the table exists and there's no requirement on the number of rows we're done here.
@@ -128,15 +119,16 @@ public class TdWaitTableOperatorFactory
 
         private boolean fetchJobResult(int rows, TDJobOperator job)
         {
-            boolean done;Optional<ArrayValue> firstRow = job.getResult(ite -> ite.hasNext() ? Optional.of(ite.next()) : Optional.absent());
+            Optional<ArrayValue> firstRow = job.getResult(ite -> ite.hasNext() ? Optional.of(ite.next()) : Optional.absent());
 
             if (!firstRow.isPresent()) {
                 throw new RuntimeException("Got unexpected empty result for count job: " + job.getJobId());
             }
-            if (firstRow.get().size() != 1) {
-                throw new RuntimeException("Got unexpected result row size for count job: " + firstRow.get().size());
+            ArrayValue row = firstRow.get();
+            if (row.size() != 1) {
+                throw new RuntimeException("Got unexpected result row size for count job: " + row.size());
             }
-            Value count = firstRow.get().get(0);
+            Value count = row.get(0);
             IntegerValue actualRows;
             try {
                 actualRows = count.asIntegerValue();
@@ -144,8 +136,8 @@ public class TdWaitTableOperatorFactory
             catch (MessageTypeCastException e) {
                 throw new RuntimeException("Got unexpected value type count job: " + count.getValueType());
             }
-            done = BigInteger.valueOf(rows).compareTo(actualRows.asBigInteger()) <= 0;
-            return done;
+
+            return BigInteger.valueOf(rows).compareTo(actualRows.asBigInteger()) <= 0;
         }
 
         private String startJob(Config params, TableParam table, int rows, String engine, TDOperator op)
@@ -188,72 +180,5 @@ public class TdWaitTableOperatorFactory
             }
             return query;
         }
-    }
-
-    private boolean poll(Config params, TableParam table, int expectedRows)
-    {
-        boolean done;
-
-        String engine = params.get("engine", String.class, "presto");
-
-        String tableName;
-        String query;
-        switch (engine) {
-            case "presto":
-                tableName = TDOperator.escapePrestoTableName(table);
-                query = "select count(*) from " + tableName;
-                break;
-            case "hive":
-                tableName = TDOperator.escapeHiveTableName(table);
-                query = "select count(*) from (select * from " + tableName + " limit " + expectedRows + ") sub";
-                break;
-            default:
-                throw new ConfigException("Unknown 'engine:' option (available options are: hive and presto): " + engine);
-        }
-
-        try (TDOperator op = TDOperator.fromConfig(params)) {
-            if (!op.tableExists(table.getTable())) {
-                return false;
-            }
-
-            if (expectedRows <= 0) {
-                return true;
-            }
-
-            int priority = params.get("priority", int.class, 0);  // TODO this should accept string (VERY_LOW, LOW, NORMAL, HIGH VERY_HIGH)
-            int jobRetry = params.get("job_retry", int.class, 0);
-
-            TDJobRequest req = new TDJobRequestBuilder()
-                    .setType(engine)
-                    .setDatabase(op.getDatabase())
-                    .setQuery(query)
-                    .setRetryLimit(jobRetry)
-                    .setPriority(priority)
-                    .createTDJobRequest();
-
-            TDJobOperator job = op.submitNewJob(req);
-            logger.info("Started {} job id={}:\n{}", engine, job.getJobId(), query);
-
-            joinJob(job);
-
-            Optional<ArrayValue> firstRow = job.getResult(ite -> ite.hasNext() ? Optional.of(ite.next()) : Optional.absent());
-
-            if (!firstRow.isPresent()) {
-                throw new RuntimeException("Got unexpected empty result for count job: " + job.getJobId());
-            }
-            if (firstRow.get().size() != 1) {
-                throw new RuntimeException("Got unexpected result row size for count job: " + firstRow.get().size());
-            }
-            Value count = firstRow.get().get(0);
-            IntegerValue actualRows;
-            try {
-                actualRows = count.asIntegerValue();
-            }
-            catch (MessageTypeCastException e) {
-                throw new RuntimeException("Got unexpected value type count job: " + count.getValueType());
-            }
-            done = BigInteger.valueOf(expectedRows).compareTo(actualRows.asBigInteger()) <= 0;
-        }
-        return done;
     }
 }
