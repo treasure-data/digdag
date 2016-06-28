b384ba317988a1af3176e08d33e380afd3527e0b
Daniel Norberg <dano@treasure-data.com>
2016-06-28 15:20:54 +0900

    add proxy support to td operators

--- digdag-standards/src/main/java/io/digdag/standards/operator/td/TDClientFactory.java
+++ digdag-standards/src/main/java/io/digdag/standards/operator/td/TDClientFactory.java
@@ -1,6 +1,9 @@
 package io.digdag.standards.operator.td;
 
+import com.google.common.base.Optional;
+import com.treasuredata.client.ProxyConfig;
 import com.treasuredata.client.TDClient;
+import com.treasuredata.client.TDClientBuilder;
 import io.digdag.client.config.Config;
 import io.digdag.client.config.ConfigException;
 
@@ -14,7 +17,15 @@ class TDClientFactory
             throw new ConfigException("Parameter 'apikey' is empty");
         }
 
-        TDClient client = TDClient.newBuilder(false)
+        TDClientBuilder builder = TDClient.newBuilder(false);
+
+        Config proxyConfig = params.getNestedOrGetEmpty("proxy");
+        boolean proxyEnabled = proxyConfig.get("enabled", Boolean.class, false);
+        if (proxyEnabled) {
+            builder.setProxy(proxyConfig(proxyConfig));
+        }
+
+        TDClient client = builder
                 .setEndpoint(params.get("endpoint", String.class, "api.treasuredata.com"))
                 .setUseSSL(params.get("use_ssl", boolean.class, true))
                 .setApiKey(apikey)
@@ -22,5 +33,32 @@ class TDClientFactory
                 .build();
 
         return client;
+    }
+
+    private static ProxyConfig proxyConfig(Config config)
+    {
+        ProxyConfig.ProxyConfigBuilder builder = new ProxyConfig.ProxyConfigBuilder();
+
+        Optional<String> host = config.getOptional("host", String.class);
+        if (host.isPresent()) {
+            builder.setHost(host.get());
+        }
+
+        Optional<Integer> port = config.getOptional("port", Integer.class);
+        if (port.isPresent()) {
+            builder.setPort(port.get());
+        }
+
+        Optional<String> user = config.getOptional("user", String.class);
+        if (user.isPresent()) {
+            builder.setUser(user.get());
+        }
+
+        Optional<String> password = config.getOptional("password", String.class);
+        if (password.isPresent()) {
+            builder.setPassword(password.get());
+        }
+
+        return builder.createProxyConfig();
     }
 }
