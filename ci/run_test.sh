dde4fb49e7a4b6e53071d7fd97ca055c11add4ef
Daniel Norberg <dano@treasure-data.com>
2016-06-29 16:19:28 +0900

    enforce min ci node count

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+export MIN_CI_NODES=3
+
 : "${CI_NODE_INDEX:?CI_NODE_INDEX required}"
+
+if [[ ${CI_NODE_TOTAL} -lt ${MIN_CI_NODES} ]]; then
+  echo "Not enough CI nodes: got ${CI_NODE_TOTAL}, need ${MIN_CI_NODES} "
+  exit 1
+fi
 
 case "${CI_NODE_INDEX}" in
 0)
