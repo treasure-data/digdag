4620b25e3af165ddab0c8386443dd27641189b0e
Mitsunori Komatsu <komamitsu@gmail.com>
2016-07-27 18:47:51 +0900

    Add PgOperatorTest

---
+++ digdag-standards/src/test/java/io/digdag/standards/operator/pg/PgOpTestHelper.java
@@ -0,0 +1,85 @@
+package io.digdag.standards.operator.pg;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Binder;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import io.digdag.client.config.Config;
+import io.digdag.client.config.ConfigFactory;
+import io.digdag.core.agent.ConfigEvalEngine;
+import io.digdag.core.config.YamlConfigLoader;
+import io.digdag.spi.ImmutableTaskRequest;
+import io.digdag.spi.TaskRequest;
+import io.digdag.spi.TemplateEngine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Map;
+import java.util.UUID;
+
+public class PgOpTestHelper
+{
+    private final ObjectMapper mapper = new ObjectMapper().registerModule(new GuavaModule());
+    private final YamlConfigLoader loader = new YamlConfigLoader();
+    private final ConfigFactory configFactory = new ConfigFactory(mapper);
+    private final Injector injector = Guice.createInjector(new MyModule());
+
+    static class MyModule implements Module
+    {
+        @Override
+        public void configure(Binder binder)
+        {
+            binder.bind(TemplateEngine.class).to(ConfigEvalEngine.class).in(Scopes.SINGLETON);
+        }
+    }
+
+    public Injector injector()
+    {
+        return injector;
+    }
+
+    public Path workpath()
+            throws IOException
+    {
+        return Files.createTempDirectory("pg_op_test");
+    }
+
+    public Config createConfig(Map<String, Object> configInput)
+            throws IOException
+    {
+        return loader.loadString(mapper.writeValueAsString(configInput)).toConfig(configFactory);
+    }
+
+    public TaskRequest createTaskRequest(Map<String, Object> configInput)
+            throws IOException
+    {
+        Config config = createConfig(configInput);
+        return ImmutableTaskRequest.
+                builder().
+                siteId(1).
+                projectId(2).
+                workflowName("wf").
+                taskId(3).
+                attemptId(4).
+                sessionId(5).
+                taskName("t").
+                queueName("q").
+                lockId("l").
+                priority(6).
+                timeZone(ZoneId.systemDefault()).
+                sessionUuid(UUID.randomUUID()).
+                sessionTime(Instant.now()).
+                createdAt(Instant.now()).
+                config(config).
+                localConfig(createConfig(ImmutableMap.of())).
+                lastStateParams(createConfig(ImmutableMap.of())).
+                build();
+    }
+}
--- digdag-standards/src/test/java/io/digdag/standards/operator/pg/PgOperatorFactoryTest.java
+++ digdag-standards/src/test/java/io/digdag/standards/operator/pg/PgOperatorFactoryTest.java
@@ -1,25 +1,13 @@
 package io.digdag.standards.operator.pg;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.inject.Binder;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Module;
-import com.google.inject.Scopes;
-import io.digdag.client.config.Config;
-import io.digdag.core.agent.ConfigEvalEngine;
-import io.digdag.spi.ImmutableTaskRequest;
 import io.digdag.spi.Operator;
-import io.digdag.spi.TemplateEngine;
+import io.digdag.spi.TaskRequest;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.time.Instant;
-import java.time.ZoneId;
 import java.util.Map;
-import java.util.UUID;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
@@ -27,22 +15,13 @@ import static org.junit.Assert.*;
 
 public class PgOperatorFactoryTest
 {
+    private PgOpTestHelper testHelper = new PgOpTestHelper();
     private PgOperatorFactory operatorFactory;
-
-    static class MyModule implements Module
-    {
-        @Override
-        public void configure(Binder binder)
-        {
-            binder.bind(TemplateEngine.class).to(ConfigEvalEngine.class).in(Scopes.SINGLETON);
-        }
-    }
 
     @Before
     public void setUp()
     {
-        Injector injector = Guice.createInjector(new MyModule());
-        operatorFactory = injector.getInstance(PgOperatorFactory.class);
+        operatorFactory = testHelper.injector().getInstance(PgOperatorFactory.class);
     }
 
     @Test
@@ -55,35 +34,13 @@ public class PgOperatorFactoryTest
     public void newTaskExecutor()
             throws IOException
     {
-        ConfigBuilder configBuilder = new ConfigBuilder();
-        Map<String, String> configInput = ImmutableMap.of(
+        Map<String, Object> configInput = ImmutableMap.of(
                 "host", "foobar0.org",
                 "user", "user0",
                 "database", "database0"
         );
-        Config config = configBuilder.createConfig(configInput);
-
-        ImmutableTaskRequest taskRequest = ImmutableTaskRequest.
-                builder().
-                siteId(1).
-                projectId(2).
-                workflowName("wf").
-                taskId(3).
-                attemptId(4).
-                sessionId(5).
-                taskName("t").
-                queueName("q").
-                lockId("l").
-                priority(6).
-                timeZone(ZoneId.systemDefault()).
-                sessionUuid(UUID.randomUUID()).
-                sessionTime(Instant.now()).
-                createdAt(Instant.now()).
-                config(config).
-                localConfig(configBuilder.createConfig(ImmutableMap.of())).
-                lastStateParams(configBuilder.createConfig(ImmutableMap.of())).
-                build();
-        Operator operator = operatorFactory.newTaskExecutor(Files.createTempDirectory("pg_op_test"), taskRequest);
+        TaskRequest taskRequest = testHelper.createTaskRequest(configInput);
+        Operator operator = operatorFactory.newTaskExecutor(testHelper.workpath(), taskRequest);
         assertThat(operator, is(instanceOf(PgOperatorFactory.PgOperator.class)));
     }
 }
