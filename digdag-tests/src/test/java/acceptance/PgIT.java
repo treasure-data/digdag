412146c33aff645b41a01c02534cb13f78676a43
Mitsunori Komatsu <komamitsu@gmail.com>
2016-08-08 19:49:48 +0900

    Add PgIt#insertIntoWithoutStrictTransaction and refactor

--- digdag-tests/src/test/java/acceptance/PgIT.java
+++ digdag-tests/src/test/java/acceptance/PgIT.java
@@ -5,6 +5,7 @@ import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
 import io.digdag.client.config.Config;
 import io.digdag.client.config.ConfigFactory;
+import io.digdag.standards.operator.jdbc.DatabaseException;
 import io.digdag.standards.operator.jdbc.NotReadOnlyException;
 import io.digdag.standards.operator.pg.PgConnection;
 import io.digdag.standards.operator.pg.PgConnectionConfig;
@@ -21,14 +22,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Path;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -39,6 +39,9 @@ import static utils.TestUtils.copyResource;
 public class PgIT
 {
     private static final String POSTGRESQL = System.getenv("DIGDAG_TEST_POSTGRESQL");
+    private static final String RESTRICTED_USER = "not_admin";
+    private static final String SRC_TABLE = "src_tbl";
+    private static final String DEST_TABLE = "dest_tbl";
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
@@ -69,6 +72,8 @@ public class PgIT
 
         createTempDatabase(props, database);
 
+        setupRestrictedUser(props, database);
+
         setupSourceTable(props, database);
     }
 
@@ -83,7 +88,7 @@ public class PgIT
             throws Exception
     {
         copyResource("acceptance/pg/select_download.dig", root().resolve("pg.dig"));
-        copyResource("acceptance/pg/select_users.sql", root().resolve("select_users.sql"));
+        copyResource("acceptance/pg/select_table.sql", root().resolve("select_table.sql"));
         TestUtils.main("run", "-o", root().toString(), "--project", root().toString(), "-p", "pg_database=" + database, "pg.dig");
 
         List<String> csvLines = new ArrayList<>();
@@ -103,12 +108,13 @@ public class PgIT
             throws Exception
     {
         copyResource("acceptance/pg/create_table.dig", root().resolve("pg.dig"));
-        copyResource("acceptance/pg/select_users.sql", root().resolve("select_users.sql"));
-        TestUtils.main("run", "-o", root().toString(), "--project", root().toString(), "-p", "pg_database=" + database, "pg.dig");
+        copyResource("acceptance/pg/select_table.sql", root().resolve("select_table.sql"));
 
         setupDestTable(props, database);
 
-        assertTableContents(props, database, "dest_tbl", Arrays.asList(
+        TestUtils.main("run", "-o", root().toString(), "--project", root().toString(), "-p", "pg_database=" + database, "pg.dig");
+
+        assertTableContents(props, database, DEST_TABLE, Arrays.asList(
                 ImmutableMap.of("id", 0, "name", "foo", "score", 3.14f),
                 ImmutableMap.of("id", 1, "name", "bar", "score", 1.23f),
                 ImmutableMap.of("id", 2, "name", "baz", "score", 5.0f)
@@ -120,12 +126,32 @@ public class PgIT
             throws Exception
     {
         copyResource("acceptance/pg/insert_into.dig", root().resolve("pg.dig"));
-        copyResource("acceptance/pg/select_users.sql", root().resolve("select_users.sql"));
-        TestUtils.main("run", "-o", root().toString(), "--project", root().toString(), "-p", "pg_database=" + database, "pg.dig");
+        copyResource("acceptance/pg/select_table.sql", root().resolve("select_table.sql"));
 
         setupDestTable(props, database);
 
-        assertTableContents(props, database, "dest_tbl", Arrays.asList(
+        TestUtils.main("run", "-o", root().toString(), "--project", root().toString(), "-p", "pg_database=" + database, "pg.dig");
+
+        assertTableContents(props, database, DEST_TABLE, Arrays.asList(
+                ImmutableMap.of("id", 0, "name", "foo", "score", 3.14f),
+                ImmutableMap.of("id", 1, "name", "bar", "score", 1.23f),
+                ImmutableMap.of("id", 2, "name", "baz", "score", 5.0f),
+                ImmutableMap.of("id", 9, "name", "zzz", "score", 9.99f)
+        ));
+    }
+
+    @Test
+    public void insertIntoWithoutStrictTransaction()
+            throws Exception
+    {
+        copyResource("acceptance/pg/insert_into_wo_st.dig", root().resolve("pg.dig"));
+        copyResource("acceptance/pg/select_table.sql", root().resolve("select_table.sql"));
+
+        setupDestTable(props, database);
+
+        TestUtils.main("run", "-o", root().toString(), "--project", root().toString(), "-p", "pg_database=" + database, "pg.dig");
+
+        assertTableContents(props, database, DEST_TABLE, Arrays.asList(
                 ImmutableMap.of("id", 0, "name", "foo", "score", 3.14f),
                 ImmutableMap.of("id", 1, "name", "bar", "score", 1.23f),
                 ImmutableMap.of("id", 2, "name", "baz", "score", 5.0f),
@@ -138,10 +164,30 @@ public class PgIT
         Config config = getDatabaseConfig(props, database);
 
         try (PgConnection conn = PgConnection.open(PgConnectionConfig.configure(config))) {
-            conn.executeUpdate("CREATE TABLE users (id integer, name text, score real)");
-            conn.executeUpdate("INSERT INTO users (id, name, score) VALUES (0, 'foo', 3.14)");
-            conn.executeUpdate("INSERT INTO users (id, name, score) VALUES (1, 'bar', 1.23)");
-            conn.executeUpdate("INSERT INTO users (id, name, score) VALUES (2, 'baz', 5.00)");
+            conn.executeUpdate("CREATE TABLE " + SRC_TABLE + " (id integer, name text, score real)");
+            conn.executeUpdate("INSERT INTO " + SRC_TABLE + " (id, name, score) VALUES (0, 'foo', 3.14)");
+            conn.executeUpdate("INSERT INTO " + SRC_TABLE + " (id, name, score) VALUES (1, 'bar', 1.23)");
+            conn.executeUpdate("INSERT INTO " + SRC_TABLE + " (id, name, score) VALUES (2, 'baz', 5.00)");
+
+            conn.executeUpdate("GRANT SELECT ON " + SRC_TABLE +  " TO " + RESTRICTED_USER);
+        }
+    }
+
+    private void setupRestrictedUser(Properties props, String database)
+    {
+        Config config = getDatabaseConfig(props, database);
+
+        try (PgConnection conn = PgConnection.open(PgConnectionConfig.configure(config))) {
+            try {
+                conn.executeUpdate("CREATE ROLE " + RESTRICTED_USER);
+            }
+            catch (DatabaseException e) {
+                // 42710: duplicate_object
+                if (!e.getCause().getSQLState().equals("42710")) {
+                    throw e;
+                }
+            }
+            conn.executeUpdate("ALTER ROLE " + RESTRICTED_USER + " LOGIN");
         }
     }
 
@@ -150,8 +196,11 @@ public class PgIT
         Config config = getDatabaseConfig(props, database);
 
         try (PgConnection conn = PgConnection.open(PgConnectionConfig.configure(config))) {
-            conn.executeUpdate("CREATE TABLE dest_tbl (id integer, name text, score real)");
-            conn.executeUpdate("INSERT INTO dest_tbl (id, name, score) VALUES (9, 'zzz', 9.99)");
+            conn.executeUpdate("CREATE TABLE IF NOT EXISTS " + DEST_TABLE + " (id integer, name text, score real)");
+            conn.executeUpdate("DELETE FROM " + DEST_TABLE + " WHERE id = 9");
+            conn.executeUpdate("INSERT INTO " + DEST_TABLE + " (id, name, score) VALUES (9, 'zzz', 9.99)");
+
+            conn.executeUpdate("GRANT INSERT ON " + DEST_TABLE +  " TO " + RESTRICTED_USER);
         }
     }
 
