85126b4d3c74f80a771d42e0e4eeecb6d9e725d9
Daniel Norberg <dano@treasure-data.com>
2016-08-29 11:42:13 +0900

    fix binding

--- digdag-standards/src/main/java/io/digdag/standards/td/TdConfigurationModule.java
+++ digdag-standards/src/main/java/io/digdag/standards/td/TdConfigurationModule.java
@@ -2,16 +2,17 @@ package io.digdag.standards.td;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import com.google.inject.name.Names;
+import com.google.inject.multibindings.Multibinder;
 import com.treasuredata.client.TDClientConfig;
 import io.digdag.spi.SecretStore;
 
-public class TdConfigurationModule implements Module
+public class TdConfigurationModule
+        implements Module
 {
     @Override
     public void configure(Binder binder)
     {
         binder.bind(TDClientConfig.class).toProvider(TdClientConfigProvider.class);
-        binder.bind(SecretStore.class).annotatedWith(Names.named("td")).to(TdConfigSecretStore.class).asEagerSingleton();
+        Multibinder.newSetBinder(binder, SecretStore.class).addBinding().to(TdConfigSecretStore.class).asEagerSingleton();
     }
 }
