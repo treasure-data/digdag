e8acb1728c035dbf379fb79719a5459e1295c2b6
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-08-30 15:02:11 -0700

    ProjectArchiveLoader should not load a directory as a workflow file

--- digdag-core/src/main/java/io/digdag/core/archive/WorkflowResourceMatcher.java
+++ digdag-core/src/main/java/io/digdag/core/archive/WorkflowResourceMatcher.java
@@ -1,22 +1,26 @@
 package io.digdag.core.archive;
 
 import java.nio.file.Path;
+import java.nio.file.Files;
 import static io.digdag.core.archive.ProjectArchive.realPathToResourceName;
 import static io.digdag.core.archive.ProjectArchive.WORKFLOW_FILE_SUFFIX;
 
 public interface WorkflowResourceMatcher
 {
-    boolean matches(String resourceName);
+    boolean matches(String resourceName, Path absolutePath);
 
     public static WorkflowResourceMatcher ofSingleFile(Path projectPath, Path workflowFilePath)
     {
         String exactResourceName = realPathToResourceName(projectPath.normalize().toAbsolutePath(), workflowFilePath.normalize().toAbsolutePath());
-        return (resourceName) -> resourceName.equals(exactResourceName);
+        return (resourceName, absolutePath) -> resourceName.equals(exactResourceName);
     }
 
     public static WorkflowResourceMatcher defaultMatcher()
     {
-        // files at current directly ending with .dig
-        return (resourceName) -> resourceName.endsWith(WORKFLOW_FILE_SUFFIX) && !resourceName.contains("/");
+        // files at the top directory ending with .dig
+        return (resourceName, absolutePath) ->
+            resourceName.endsWith(WORKFLOW_FILE_SUFFIX)
+            && !resourceName.contains("/")
+            && Files.isRegularFile(absolutePath);
     }
 }
