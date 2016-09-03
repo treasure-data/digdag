54d28d5e4763e7f46fb47e602f21c693096aac59
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-09-02 18:50:12 -0700

    style fix

--- digdag-server/src/main/java/io/digdag/server/WorkflowExecutorLoop.java
+++ digdag-server/src/main/java/io/digdag/server/WorkflowExecutorLoop.java
@@ -100,7 +100,7 @@ public class WorkflowExecutorLoop
 
     private void startShutdown()
     {
-        if (stop == false) {
+        if (!stop) {
             stop = true;
             logger.info("Shutting down workflow executor loop");
 
