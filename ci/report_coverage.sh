fef6c872a8af4aeb5bd3c722176cd937e49700c3
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-09-15 10:57:01 -0700

    coverall gradle plugin needs travis environment variables

--- ci/report_coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-docker run \
--w /digdag \
--v `pwd`/:/digdag \
--v ~/.gradle:/root/.gradle \
--e TERM=dumb \
-digdag-build \
-./gradlew jacocoTestReport coveralls --info --no-daemon "$@"
