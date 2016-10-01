b27ae24c25264b60947e2807e562341e5f4964fe
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-09-30 17:04:54 -0700

    Allow ${...} syntax return objects and arrays

--- digdag-core/src/main/resources/io/digdag/core/agent/digdag.js
+++ digdag-core/src/main/resources/io/digdag/core/agent/digdag.js
@@ -1,9 +1,32 @@
+function scopedEvaluate(code, variables)
+{
+  var source = 'with(this){\n' + code + '}\n';
+
+  try {
+    var func = new Function(source);
+  } catch (e) {
+    e.source = source;
+    throw e;
+  }
+
+  if (typeof variables == "string") {
+    variables = JSON.parse(variables);
+  }
+  vs = func.call(variables);
+
+  return vs;
+}
+
+function jsonEvaluate(code, variables)
+{
+    return scopedEvaluate('return JSON.stringify((' + code + '))', variables);
+}
+
 // Code from Underscore.js
 function template(code, variables)
 {
   var matcher = RegExp([
-    (/\$(?!\$){%([\s\S]+?)%}/g).source,
-    (/\$(?!\$){([\s\S]+?)}/g).source
+    (/\${([\s\S]+?)}/g).source
   ].join('|') + '|$', 'g');
 
   var escapes = {
@@ -23,13 +46,11 @@ function template(code, variables)
 
   var index = 0;
   var source = "__p+='";
-  code.replace(matcher, function(match, statement, expression, offset) {
+  code.replace(matcher, function(match, expression, offset) {
     source += code.slice(index, offset).replace(/\$\$/g, "$").replace(escaper, escapeChar);
     index = offset + match.length;
 
-    if (statement) {
-      source += "';\n" + evaluate + "\n__p+='";
-    } else if (expression) {
+    if (expression) {
       source += "'+\n((__t=(" + expression + "))==null?'':(typeof __t==\"string\"?__t:JSON.stringify(__t)))+\n'";
     }
 
@@ -41,19 +62,5 @@ function template(code, variables)
     "print=function(){__p+=__j.call(arguments,'');};\n" +
     source + 'return __p;\n';
 
-  source = 'with(this){\n' + source + '}\n';
-
-  try {
-    var func = new Function(source);
-  } catch (e) {
-    e.source = source;
-    throw e;
-  }
-
-  if (typeof variables == "string") {
-    variables = JSON.parse(variables);
-  }
-  vs = func.call(variables);
-
-  return vs;
+  return scopedEvaluate(source, variables);
 }
