89f6157846a9c66c80bc3c828962242e63e79b03
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-10-10 11:28:13 -0700

    fixed PluginIT

--- digdag-tests/src/test/resources/acceptance/plugin/digdag-plugin-example/src/main/java/io/digdag/plugin/example/ExampleOperatorFactory.java
+++ digdag-tests/src/test/resources/acceptance/plugin/digdag-plugin-example/src/main/java/io/digdag/plugin/example/ExampleOperatorFactory.java
@@ -1,17 +1,17 @@
 package io.digdag.plugin.example;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import com.google.common.base.Throwables;
 import io.digdag.client.config.Config;
 import io.digdag.spi.Operator;
+import io.digdag.spi.OperatorContext;
 import io.digdag.spi.OperatorFactory;
 import io.digdag.spi.TaskRequest;
 import io.digdag.spi.TaskResult;
 import io.digdag.spi.TemplateEngine;
-import io.digdag.spi.OperatorContext;
 import io.digdag.util.BaseOperator;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ExampleOperatorFactory
