085e5ea41b1db5c1df826c2fcd650637dc18011d
Daniel Norberg <dano@treasure-data.com>
2016-11-09 10:55:05 +0900

    remove unnecessary import

--- digdag-guice-rs-server/src/main/java/io/digdag/guice/rs/server/ServerBootstrap.java
+++ digdag-guice-rs-server/src/main/java/io/digdag/guice/rs/server/ServerBootstrap.java
@@ -2,9 +2,6 @@ package io.digdag.guice.rs.server;
 
 import org.embulk.guice.Bootstrap;
 
-import java.net.InetSocketAddress;
-import java.util.List;
-
 public interface ServerBootstrap
 {
     Bootstrap bootstrap();
