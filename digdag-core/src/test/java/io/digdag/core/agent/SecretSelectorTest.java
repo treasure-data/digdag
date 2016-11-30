e08a456621a200795e66c19bae9c00b207397b7e
Daniel Norberg <dano@treasure-data.com>
2016-11-30 15:43:41 +0900

    allow '*' secret selector

--- digdag-core/src/test/java/io/digdag/core/agent/SecretSelectorTest.java
+++ digdag-core/src/test/java/io/digdag/core/agent/SecretSelectorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class SecretSelectorTest
 {
@@ -12,10 +13,40 @@ public class SecretSelectorTest
     public void match()
             throws Exception
     {
+        assertThat(SecretSelector.of("*").match("foo"), is(true));
+        assertThat(SecretSelector.of("*").match("bar"), is(true));
         assertThat(SecretSelector.of("foo").match("foo"), is(true));
         assertThat(SecretSelector.of("foo").match("bar"), is(false));
         assertThat(SecretSelector.of("foo.*").match("foo.bar"), is(true));
         assertThat(SecretSelector.of("foo.*").match("foo"), is(false));
         assertThat(SecretSelector.of("foo.*").match("bar"), is(false));
+    }
+
+    @Test
+    public void testValidation()
+            throws Exception
+    {
+        assertValidSelector("*");
+        assertValidSelector("f.*");
+        assertValidSelector("foo.*");
+        assertInvalidSelector("");
+        assertInvalidSelector(".");
+        assertInvalidSelector(".*");
+        assertInvalidSelector("foo.bar*");
+        assertInvalidSelector("foo.b*");
+    }
+
+    private void assertInvalidSelector(String s)
+    {
+        try {
+            SecretSelector.of(s);
+            fail();
+        } catch (IllegalArgumentException ignore) {
+        }
+    }
+
+    private void assertValidSelector(String s)
+    {
+        SecretSelector.of(s);
     }
 }
