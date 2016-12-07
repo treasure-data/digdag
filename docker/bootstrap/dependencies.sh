5ef52269803cb7cee66b54e01f81a56479f123a6
Daniele Zannotti <d.zannotti@me.com>
2016-12-07 18:18:26 +0000

    Instal yarn in docker

--- docker/bootstrap/dependencies.sh
+++ docker/bootstrap/dependencies.sh
@@ -9,7 +9,10 @@ apt-get -y install maven
 apt-get -y install curl
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get -y install nodejs
-npm install -g yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 
 # Postgres
 add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
