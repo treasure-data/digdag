c08b122fdb70c6774bbf7a1e29d4d204c63092e3
Daniele Zannotti <d.zannotti@me.com>
2016-12-07 17:50:17 +0000

    add yarn to docker install

--- docker/bootstrap/dependencies.sh
+++ docker/bootstrap/dependencies.sh
@@ -9,6 +9,7 @@ apt-get -y install maven
 apt-get -y install curl
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get -y install nodejs
+npm install -g yarn
 
 # Postgres
 add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
