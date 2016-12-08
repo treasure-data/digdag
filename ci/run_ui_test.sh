bf344e965488b0fb3fc17fe1b3afd8bf10588c02
Daniele Zannotti <d.zannotti@me.com>
2016-12-08 19:50:51 +0000

    Added yarn install to run

--- ci/run_ui_test.sh
+++ ci/run_ui_test.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -xe
 
 docker run \
--w /digdag/digdag-ui \
--v `pwd`/:/digdag \
--v ~/.gradle:/root/.gradle \
--e TD_API_KEY="${TD_API_KEY}" \
--e TD_LOAD_IT_SFTP_USER="${TD_LOAD_IT_SFTP_USER}" \
--e TD_LOAD_IT_SFTP_PASSWORD="${TD_LOAD_IT_SFTP_PASSWORD}" \
--e GCP_CREDENTIAL="${GCP_CREDENTIAL}" \
--e GCS_TEST_BUCKET="${GCS_TEST_BUCKET}" \
--e CI_NODE_TOTAL="${CI_NODE_TOTAL}" \
--e CI_NODE_INDEX="${CI_NODE_INDEX}" \
--e CI_ACCEPTANCE_TEST=true \
--e TERM=dumb \
-$BUILD_IMAGE \
-yarn test
+  -w /digdag/digdag-ui \
+  -v `pwd`/:/digdag \
+  -v ~/.gradle:/root/.gradle \
+  -e TD_API_KEY="${TD_API_KEY}" \
+  -e TD_LOAD_IT_SFTP_USER="${TD_LOAD_IT_SFTP_USER}" \
+  -e TD_LOAD_IT_SFTP_PASSWORD="${TD_LOAD_IT_SFTP_PASSWORD}" \
+  -e GCP_CREDENTIAL="${GCP_CREDENTIAL}" \
+  -e GCS_TEST_BUCKET="${GCS_TEST_BUCKET}" \
+  -e CI_NODE_TOTAL="${CI_NODE_TOTAL}" \
+  -e CI_NODE_INDEX="${CI_NODE_INDEX}" \
+  -e CI_ACCEPTANCE_TEST=true \
+  -e TERM=dumb \
+  $BUILD_IMAGE \ yarn && yarn test
