ec21947f8a435d31a3b1f4fbe6224821a1065e87
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-12-08 00:46:51 -0800

    Add rename_tables option to td_ddl operator

--- digdag-standards/src/main/java/io/digdag/standards/operator/td/TdDdlOperatorFactory.java
+++ digdag-standards/src/main/java/io/digdag/standards/operator/td/TdDdlOperatorFactory.java
@@ -1,8 +1,10 @@
 package io.digdag.standards.operator.td;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import io.digdag.client.config.Config;
+import io.digdag.client.config.ConfigException;
 import io.digdag.core.Environment;
 import io.digdag.spi.Operator;
 import io.digdag.spi.OperatorFactory;
@@ -14,6 +16,7 @@ import io.digdag.spi.TaskResult;
 import io.digdag.standards.operator.DurationInterval;
 import io.digdag.standards.operator.state.TaskState;
 import io.digdag.util.BaseOperator;
+import org.immutables.value.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +26,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import static io.digdag.standards.operator.td.BaseTdJobOperator.configSelectorBuilder;
 import static com.google.common.collect.Iterables.concat;
 import static io.digdag.standards.operator.state.PollingRetryExecutor.pollingRetryExecutor;
+import static io.digdag.standards.operator.td.BaseTdJobOperator.configSelectorBuilder;
+import static java.util.Locale.ENGLISH;
 
 public class TdDdlOperatorFactory
         implements OperatorFactory
@@ -84,6 +88,8 @@ public class TdDdlOperatorFactory
             List<TableParam> createTableList = params.getListOrEmpty("create_tables", TableParam.class);
             List<TableParam> emptyTableList = params.getListOrEmpty("empty_tables", TableParam.class);
 
+            List<RenameTableConfig> renameTableList = params.getListOrEmpty("rename_tables", RenameTableConfig.class);
+
             List<Consumer<TDOperator>> operations = new ArrayList<>();
 
             for (String d : concat(dropDatabaseList, emptyDatabaseList)) {
@@ -110,8 +116,34 @@ public class TdDdlOperatorFactory
                     op.withDatabase(t.getDatabase().or(op.getDatabase())).ensureTableCreated(t.getTable());
                 });
             }
+            for (RenameTableConfig r : renameTableList) {
+                TableParam from = r.getFromTable();
+                String to = r.getToTable();
+                if (to.contains(".")) {
+                    throw new ConfigException("'to' option of rename_tables must not include database name");
+                }
+                operations.add(op -> {
+                    logger.info("Renaming TD table {}.{} -> {}", op.getDatabase(), from, to);
+                    op.withDatabase(from.getDatabase().or(op.getDatabase())).ensureExistentTableRenamed(from.getTable(), to);
+                });
+            }
 
             try (TDOperator op = TDOperator.fromConfig(env, params, context.getSecrets().getSecrets("td"))) {
+                // make sure that all "from" tables exist so that ignoring 404 Not Found in
+                // op.ensureExistentTableRenamed is valid.
+                boolean renameFromChecked = state.params().get("rename_from_checked", boolean.class, false);
+                if (!renameFromChecked && !renameTableList.isEmpty()) {
+                    for (RenameTableConfig r : renameTableList) {
+                        TableParam from = r.getFromTable();
+                        String database = from.getDatabase().or(op.getDatabase());
+                        if (!op.withDatabase(database).tableExists(from.getTable())) {
+                            throw new ConfigException(String.format(ENGLISH,
+                                        "Renaming table %s doesn't exist", database, r.getFromTable().getTable()));
+                        }
+                    }
+                    state.params().set("rename_from_checked", true);
+                }
+
                 int operation = state.params().get("operation", int.class, 0);
                 for (int i = operation; i < operations.size(); i++) {
                     state.params().set("operation", i);
@@ -124,11 +156,21 @@ public class TdDdlOperatorFactory
                             .withRetryInterval(retryInterval)
                             .withErrorMessage("DDL operation failed")
                             .runAction(s -> o.accept(op));
-
                 }
             }
 
             return TaskResult.empty(request);
         }
+    }
+
+    @Value.Immutable
+    @Value.Style(visibility = Value.Style.ImplementationVisibility.PACKAGE)
+    interface RenameTableConfig
+    {
+        @JsonProperty("from")
+        TableParam getFromTable();
+
+        @JsonProperty("to")
+        String getToTable();
     }
 }
