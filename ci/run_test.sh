f67a70073cb406b7b5d6d7da2b26afa0cb9db825
Daniel Norberg <dano@treasure-data.com>
2016-12-08 12:39:12 +0900

    debug

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ $(( 0 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
-    "${BASEDIR}/run_findbugs.sh"
-fi
-
-if [[ $(( 1 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
-    "${BASEDIR}/validate.sh"
-fi
+# if [[ $(( 0 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
+#     "${BASEDIR}/run_findbugs.sh"
+# fi
+#
+# if [[ $(( 1 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
+#     "${BASEDIR}/validate.sh"
+# fi
 
 "${BASEDIR}/run_test_h2.sh"
 "${BASEDIR}/run_test_pg.sh"
