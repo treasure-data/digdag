7b8e2495ec847e224540e38af2e6939093ba67be
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-12-12 11:42:11 -0800

    add failure test cases to download command

--- digdag-tests/src/test/java/acceptance/DownloadIT.java
+++ digdag-tests/src/test/java/acceptance/DownloadIT.java
@@ -22,6 +22,7 @@ import utils.TemporaryDigdagServer;
 import static utils.TestUtils.copyResource;
 import static utils.TestUtils.main;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 
@@ -48,25 +49,7 @@ public class DownloadIT
     public void download()
             throws Exception
     {
-        // Create new project
-        CommandStatus initStatus = main("init",
-                "-c", config.toString(),
-                projectDir.toString());
-        assertThat(initStatus.code(), is(0));
-
-        Files.createDirectories(projectDir.resolve("files"));
-
-        copyResource("acceptance/basic.dig", projectDir.resolve("basic.dig"));
-        copyResource("acceptance/params.dig", projectDir.resolve("files").resolve("params.yml"));
-
-        // Push the project
-        CommandStatus pushStatus = main("push",
-                "--project", projectDir.toString(),
-                "download_proj",
-                "-c", config.toString(),
-                "-e", server.endpoint(),
-                "-r", "rev4711");
-        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
+        pushProject("download_proj", "rev4711");
 
         // Download the project
         Path downloadDir = projectDir.resolve("extract");
@@ -81,6 +64,62 @@ public class DownloadIT
         assertThat(Files.readAllBytes(downloadDir.resolve("files").resolve("params.yml")), is(readResource("acceptance/params.dig")));
 
         assertThat(downloadStatus.outUtf8(), containsString("rev4711"));
+    }
+
+    @Test
+    public void projectNotFound()
+            throws Exception
+    {
+        Path downloadDir = projectDir.resolve("extract");
+        CommandStatus downloadStatus = main("download",
+                "download_proj",
+                "-o", downloadDir.toString(),
+                "-c", config.toString(),
+                "-e", server.endpoint());
+        assertThat(downloadStatus.errUtf8(), downloadStatus.code(), not(0));
+        assertThat(downloadStatus.errUtf8(), containsString("project not found: download_proj"));
+    }
+
+    @Test
+    public void revisionNotFound()
+            throws Exception
+    {
+        pushProject("download_proj", "rev4711");
+
+        // Download the project with -r option
+        Path downloadDir = projectDir.resolve("extract");
+        CommandStatus downloadStatus = main("download",
+                "download_proj",
+                "-o", downloadDir.toString(),
+                "-r", "no_such_revision",
+                "-c", config.toString(),
+                "-e", server.endpoint());
+        assertThat(downloadStatus.errUtf8(), downloadStatus.code(), not(0));
+        assertThat(downloadStatus.errUtf8(), containsString("Project archive of revision 'no_such_revision' does not exist."));
+    }
+
+    private void pushProject(String projectName, String revisionName)
+            throws Exception
+    {
+        // Create new project
+        CommandStatus initStatus = main("init",
+                "-c", config.toString(),
+                projectDir.toString());
+        assertThat(initStatus.code(), is(0));
+
+        Files.createDirectories(projectDir.resolve("files"));
+
+        copyResource("acceptance/basic.dig", projectDir.resolve("basic.dig"));
+        copyResource("acceptance/params.dig", projectDir.resolve("files").resolve("params.yml"));
+
+        // Push the project
+        CommandStatus pushStatus = main("push",
+                "--project", projectDir.toString(),
+                projectName,
+                "-c", config.toString(),
+                "-e", server.endpoint(),
+                "-r", revisionName);
+        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
     }
 
     private byte[] readResource(String resource)
