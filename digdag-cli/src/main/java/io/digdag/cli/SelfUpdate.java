572807df8f0de7282f74796bba967c21bb0c0071
Daniel Norberg <dano@treasure-data.com>
2016-12-14 21:44:55 +0900

    release 0.9.1

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -55,7 +55,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ " + programName + " selfupdate");
-        err.println("    $ " + programName + " selfupdate 0.9.1-SNAPSHOT");
+        err.println("    $ " + programName + " selfupdate 0.9.1");
         err.println("");
         return systemExit(error);
     }
---
+++ digdag-docs/src/releases/release-0.9.1.rst
@@ -0,0 +1,8 @@
+Release 0.9.1
+=============
+
+Version bug fix release.
+
+Release Date
+------------
+2016-12-14
