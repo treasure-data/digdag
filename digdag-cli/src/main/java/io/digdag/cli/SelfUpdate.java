d3a72d44df98c61e2e175d0dcb8652ee172817ed
Daniel Norberg <dano@treasure-data.com>
2017-01-11 15:38:33 +0900

    bump version

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -90,7 +90,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ " + programName + " selfupdate");
-        err.println("    $ " + programName + " selfupdate 0.9.2");
+        err.println("    $ " + programName + " selfupdate 0.9.3-SNAPSHOT");
         err.println("");
         return systemExit(error);
     }
