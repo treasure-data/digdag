25ddfa7bd20826a5b3fecc5ad14a44539077d987
Sadayuki Furuhashi <frsyuki@gmail.com>
2017-03-14 22:48:42 -0700

    start v0.9.8-SNAPSHOT

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -90,7 +90,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ " + programName + " selfupdate");
-        err.println("    $ " + programName + " selfupdate 0.9.7");
+        err.println("    $ " + programName + " selfupdate 0.9.8-SNAPSHOT");
         err.println("");
         return systemExit(error);
     }
