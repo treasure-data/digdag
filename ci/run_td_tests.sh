b2340f7ec765e8f09e2f26858054e9494e8f2bdf
Mitsunori Komatsu <komamitsu@gmail.com>
2017-06-06 13:31:16 +0900

    Limit test cases only to BigQueryIT to reduce testing time
    (need to revert this change)

--- ci/run_td_tests.sh
+++ ci/run_td_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"${BASEDIR}"/run_test_pg.sh -p digdag-tests --tests 'acceptance.td.*'
+"${BASEDIR}"/run_test_pg.sh -p digdag-tests --tests 'acceptance.td.BigQueryIT'
 
