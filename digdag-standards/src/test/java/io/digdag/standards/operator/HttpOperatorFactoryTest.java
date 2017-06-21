b5405146a731be29fe4058df8c0499deac61e4eb
shunsuke maeda <duck8823@gmail.com>
2017-06-22 06:58:54 +0900

    Use random port

--- digdag-standards/src/test/java/io/digdag/standards/operator/HttpOperatorFactoryTest.java
+++ digdag-standards/src/test/java/io/digdag/standards/operator/HttpOperatorFactoryTest.java
@@ -1,6 +1,7 @@
 package io.digdag.standards.operator;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.digdag.client.config.Config;
 import io.digdag.spi.Operator;
 import io.digdag.spi.TaskExecutionException;
 import org.junit.Before;
@@ -14,10 +15,10 @@ import java.nio.file.Path;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static io.digdag.client.config.ConfigUtils.newConfig;
 import static io.digdag.core.workflow.OperatorTestingUtils.newContext;
 import static io.digdag.core.workflow.OperatorTestingUtils.newOperatorFactory;
 import static io.digdag.core.workflow.OperatorTestingUtils.newTaskRequest;
-import static io.digdag.core.workflow.WorkflowTestingUtils.loadYamlResource;
 import static org.junit.Assert.fail;
 
 public class HttpOperatorFactoryTest
@@ -26,16 +27,24 @@ public class HttpOperatorFactoryTest
 	public TemporaryFolder folder = new TemporaryFolder();
 
 	@Rule
-	public WireMockRule wireMockRule = new WireMockRule(8089);
+	public WireMockRule wireMockRule = new WireMockRule(0);
 
 	private Path tempPath;
 	private HttpOperatorFactory factory;
+	private Config config;
 
 	@Before
 	public void createInstance()
 	{
 		this.tempPath = folder.getRoot().toPath();
 		this.factory = newOperatorFactory(HttpOperatorFactory.class);
+
+		Config config = newConfig();
+		config.set("_command", "http://localhost:" + wireMockRule.port() + "/api/foobar");
+		config.set("timeout", 3);
+		config.set("retry", false);
+
+		this.config = config;
 	}
 
 
@@ -48,8 +57,7 @@ public class HttpOperatorFactoryTest
 
 		Operator op = factory.newOperator(newContext(
 				tempPath,
-				newTaskRequest().withConfig(
-						loadYamlResource("/io/digdag/standards/operator/http/basic.yml"))));
+				newTaskRequest().withConfig(config)));
 		try {
 			op.run();
 			fail("should be thrown Exception.");
@@ -66,8 +74,7 @@ public class HttpOperatorFactoryTest
 
 		Operator op = factory.newOperator(newContext(
 				tempPath,
-				newTaskRequest().withConfig(
-						loadYamlResource("/io/digdag/standards/operator/http/basic.yml"))));
+				newTaskRequest().withConfig(config)));
 		try {
 			op.run();
 		} catch (TaskExecutionException ignore) {
