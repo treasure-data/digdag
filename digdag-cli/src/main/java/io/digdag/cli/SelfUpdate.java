5e1199a8053f9feca61d2b25a8ee318d6641074d
Mitsunori Komatsu <komamitsu@gmail.com>
2017-09-04 13:36:43 +0900

    Start 0.9.17-SNAPSHOT

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -90,7 +90,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ " + programName + " selfupdate");
-        err.println("    $ " + programName + " selfupdate 0.9.16");
+        err.println("    $ " + programName + " selfupdate 0.9.17-SNAPSHOT");
         err.println("");
         return systemExit(error);
     }
