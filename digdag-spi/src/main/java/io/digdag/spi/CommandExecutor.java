e37a24cbb41f0d65815069ba8d3f5f3e279f6865
Muga Nishizawa <muga.nishizawa@gmail.com>
2018-06-22 01:26:28 -0700

    Change spi.CommandExecutor SPI v3

--- digdag-spi/src/main/java/io/digdag/spi/CommandExecutor.java
+++ digdag-spi/src/main/java/io/digdag/spi/CommandExecutor.java
@@ -1,18 +1,29 @@
 package io.digdag.spi;
 
+import io.digdag.client.config.Config;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 
-public interface CommandExecutor<T extends CommandContext>
+public interface CommandExecutor
 {
     @Deprecated
     Process start(Path projectPath, TaskRequest request, ProcessBuilder pb)
             throws IOException;
 
-    CommandResult start(T context)
-            throws IOException;
+    CommandStatus run(Path projectPath,
+            Path workspacePath,
+            TaskRequest request,
+            Map<String, String> environments,
+            List<String> cmdline,
+            Map<String, CommandExecutorContent> inputContents,   // {path => content}
+            CommandExecutorContent outputContent)                // {content}
+            throws IOException, InterruptedException;
 
-    // Be used for non-blocking tasks polling
-    CommandResult get(CommandState state)
-            throws IOException;
+    // this method is used for non-blocking task polling
+    CommandStatus poll(Path projectPath,
+            TaskRequest request,
+            String commandId,
+            Config executorState);
 }
