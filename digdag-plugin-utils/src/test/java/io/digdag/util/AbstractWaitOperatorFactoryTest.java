3079512a161650c40080cc4c9e6800ad5c6dedd4
Muga Nishizawa <muga.nishizawa@gmail.com>
2018-06-27 16:40:55 -0700

    Refacotr and move simple test util to digdag-plugin-utils as well

--- digdag-plugin-utils/src/test/java/io/digdag/util/AbstractWaitOperatorFactoryTest.java
+++ digdag-plugin-utils/src/test/java/io/digdag/util/AbstractWaitOperatorFactoryTest.java
@@ -8,11 +8,13 @@ import io.digdag.client.config.Config;
 import io.digdag.client.config.ConfigException;
 import io.digdag.client.config.ConfigFactory;
 import io.digdag.util.AbstractWaitOperatorFactory;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Map;
+import java.util.concurrent.Callable;
 
-import static io.digdag.standards.operator.td.Utils.assertThrows;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -30,9 +32,9 @@ public class AbstractWaitOperatorFactoryTest
     {
         // Check value bounds / parsing
         String[] keys = {
-                "config.td.wait.poll_interval",
-                "config.td.wait.min_poll_interval",
-                "config.td.wait.max_poll_interval"
+                "config.test.wait.poll_interval",
+                "config.test.wait.min_poll_interval",
+                "config.test.wait.max_poll_interval"
         };
         String[] illegalValues = {
                 "",
@@ -48,78 +50,78 @@ public class AbstractWaitOperatorFactoryTest
         }
 
         // Check that the min/max bounds apply to the configured default poll interval
-        assertConfigurationFailsValidation(ImmutableMap.of("config.td.wait.poll_interval", "5s"));
-        assertConfigurationFailsValidation(ImmutableMap.of("config.td.wait.poll_interval", "29s"));
-        assertConfigurationPassesValidation(ImmutableMap.of("config.td.wait.poll_interval", "30s"));
-        assertConfigurationPassesValidation(ImmutableMap.of("config.td.wait.poll_interval", "31s"));
+        assertConfigurationFailsValidation(ImmutableMap.of("config.test.wait.poll_interval", "5s"));
+        assertConfigurationFailsValidation(ImmutableMap.of("config.test.wait.poll_interval", "29s"));
+        assertConfigurationPassesValidation(ImmutableMap.of("config.test.wait.poll_interval", "30s"));
+        assertConfigurationPassesValidation(ImmutableMap.of("config.test.wait.poll_interval", "31s"));
 
 
         // Check that the default poll interval is adjusted to fit min/max
         {
             AbstractWaitOperatorFactory f = assertConfigurationPassesValidation(ImmutableMap.of(
-                    "config.td.wait.min_poll_interval", "45s",
-                    "config.td.wait.max_poll_interval", "60s"
+                    "config.test.wait.min_poll_interval", "45s",
+                    "config.test.wait.max_poll_interval", "60s"
             ));
             assertThat(f.getPollInterval(CONFIG_FACTORY.create()), is(45));
         }
 
         // fail: poll < min
         assertConfigurationFailsValidation(ImmutableMap.of(
-                "config.td.wait.min_poll_interval", "10s",
-                "config.td.wait.poll_interval", "5s"
+                "config.test.wait.min_poll_interval", "10s",
+                "config.test.wait.poll_interval", "5s"
         ));
 
         // fail: poll > max
         assertConfigurationFailsValidation(ImmutableMap.of(
-                "config.td.wait.min_poll_interval", "30s",
-                "config.td.wait.max_poll_interval", "40s",
-                "config.td.wait.poll_interval", "50s"
+                "config.test.wait.min_poll_interval", "30s",
+                "config.test.wait.max_poll_interval", "40s",
+                "config.test.wait.poll_interval", "50s"
         ));
 
         // fail: max < min
         assertConfigurationFailsValidation(ImmutableMap.of(
-                "config.td.wait.min_poll_interval", "10s",
-                "config.td.wait.max_poll_interval", "5s"
+                "config.test.wait.min_poll_interval", "10s",
+                "config.test.wait.max_poll_interval", "5s"
         ));
 
         {
             AbstractWaitOperatorFactory f = assertConfigurationPassesValidation(ImmutableMap.of(
-                    "config.td.wait.min_poll_interval", "5s",
-                    "config.td.wait.poll_interval", "5s",
-                    "config.td.wait.max_poll_interval", "5s"
+                    "config.test.wait.min_poll_interval", "5s",
+                    "config.test.wait.poll_interval", "5s",
+                    "config.test.wait.max_poll_interval", "5s"
             ));
             assertThat(f.getPollInterval(CONFIG_FACTORY.create()), is(5));
-            Utils.assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "3s")))), ConfigException.class);
-            Utils.assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "7s")))), ConfigException.class);
+            assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "3s")))), ConfigException.class);
+            assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "7s")))), ConfigException.class);
         }
 
         {
             AbstractWaitOperatorFactory f = assertConfigurationPassesValidation(ImmutableMap.of(
-                    "config.td.wait.min_poll_interval", "5s",
-                    "config.td.wait.poll_interval", "10s",
-                    "config.td.wait.max_poll_interval", "15s"
+                    "config.test.wait.min_poll_interval", "5s",
+                    "config.test.wait.poll_interval", "10s",
+                    "config.test.wait.max_poll_interval", "15s"
             ));
             assertThat(f.getPollInterval(CONFIG_FACTORY.create()), is(10));
-            Utils.assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "3s")))), ConfigException.class);
-            Utils.assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "30s")))), ConfigException.class);
+            assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "3s")))), ConfigException.class);
+            assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "30s")))), ConfigException.class);
         }
 
         {
             AbstractWaitOperatorFactory f = assertConfigurationPassesValidation(ImmutableMap.of(
-                    "config.td.wait.min_poll_interval", "10s",
-                    "config.td.wait.poll_interval", "5m",
-                    "config.td.wait.max_poll_interval", "2h"
+                    "config.test.wait.min_poll_interval", "10s",
+                    "config.test.wait.poll_interval", "5m",
+                    "config.test.wait.max_poll_interval", "2h"
             ));
             assertThat(f.getPollInterval(CONFIG_FACTORY.create()), is(5 * 60));
-            Utils.assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "5s")))), ConfigException.class);
-            Utils.assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "3h")))), ConfigException.class);
+            assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "5s")))), ConfigException.class);
+            assertThrows(() -> f.getPollInterval(CONFIG_FACTORY.create(cfg(ImmutableMap.of("interval", "3h")))), ConfigException.class);
         }
     }
 
     private void assertConfigurationFailsValidation(Map<String, Object> config)
             throws JsonProcessingException
     {
-        Utils.assertThrows(() -> new AbstractWaitOperatorFactory(cfg(config)) {}, ConfigException.class);
+        assertThrows(() -> new AbstractWaitOperatorFactory("test.wait", cfg(config)) {}, ConfigException.class);
     }
 
     private Config cfg(Map<String, Object> config)
@@ -133,6 +135,18 @@ public class AbstractWaitOperatorFactoryTest
             throws JsonProcessingException
     {
         Config c = cfg(config);
-        return new AbstractWaitOperatorFactory(c) {};
+        return new AbstractWaitOperatorFactory("test.wait", c) {};
+    }
+
+    static <T, E extends Throwable> void assertThrows(Callable<T> callable, Class<E> exceptionClass)
+    {
+        try {
+            callable.call();
+        }
+        catch (Throwable e) {
+            assertThat(e, Matchers.instanceOf(exceptionClass));
+            return;
+        }
+        Assert.fail("Expected an exception of type: " + exceptionClass);
     }
 }
