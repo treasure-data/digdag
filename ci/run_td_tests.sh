be1398bb167effd05a629f79fc18a30e12e62d62
Muga Nishizawa <muga.nishizawa@gmail.com>
2018-08-10 15:14:25 -0700

    Fix ci/run_td_tests.sh to make it simple more

--- ci/run_td_tests.sh
+++ ci/run_td_tests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"${BASEDIR}"/run_test_pg.sh -p digdag-tests --tests 'acceptance.td.*'
+export DIGDAG_TEST_POSTGRESQL="
+host = localhost
+port = 5432
+user = digdag_test
+password =
+database = digdag_test
+idleTimeout = 10
+minimumPoolSize = 0
+"
 
+./gradlew clean cleanTest test --info --no-daemon -p digdag-tests --tests 'acceptance.td.*'
