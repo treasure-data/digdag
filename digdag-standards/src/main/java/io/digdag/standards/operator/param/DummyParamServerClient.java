0bb6d30ca5204f30557883662265c9cc6c4797ba
Kazuhiro Serizawa <nserihiro@gmail.com>
2018-09-21 11:41:07 -0700

    Add warn log at DummyParamServerClient

--- digdag-standards/src/main/java/io/digdag/standards/operator/param/DummyParamServerClient.java
+++ digdag-standards/src/main/java/io/digdag/standards/operator/param/DummyParamServerClient.java
@@ -5,12 +5,16 @@ import com.google.common.base.Optional;
 import io.digdag.spi.ParamServerClient;
 import io.digdag.spi.ParamServerClientConnection;
 import io.digdag.spi.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Consumer;
 
 public class DummyParamServerClient
         implements ParamServerClient
 {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     public DummyParamServerClient(ParamServerClientConnection _connection, ObjectMapper _objectMapper)
     {
 
@@ -19,13 +23,14 @@ public class DummyParamServerClient
     @Override
     public Optional<Record> get(String key, int sitedId)
     {
+        logger.warn("This is a Dummy Implementation, so just will return null");
         return null;
     }
 
     @Override
     public void set(String key, String value, int siteId)
     {
-
+        logger.warn("This is a Dummy Implementation, so no value is set");
     }
 
     @Override
