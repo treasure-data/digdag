64348e67e0a8f62f400c022c56bef16066519a8a
Hiroyuki Sato <hiroysato@gmail.com>
2018-10-20 21:52:04 +0900

    Use GreenMail instead of Wiser. GreenMail has many assertion methods.

--- digdag-standards/src/test/java/io/digdag/standards/operator/MailOperatorFactoryTest.java
+++ digdag-standards/src/test/java/io/digdag/standards/operator/MailOperatorFactoryTest.java
@@ -1,6 +1,9 @@
 package io.digdag.standards.operator;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.Resources;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import io.digdag.client.config.Config;
 import io.digdag.spi.Operator;
 import org.junit.After;
@@ -10,7 +13,8 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.subethamail.wiser.Wiser;
+
+import javax.mail.internet.InternetAddress;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,23 +22,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static io.digdag.client.config.ConfigUtils.newConfig;
+import static io.digdag.core.workflow.OperatorTestingUtils.newContext;
 import static io.digdag.core.workflow.OperatorTestingUtils.newOperatorFactory;
 import static io.digdag.core.workflow.OperatorTestingUtils.newTaskRequest;
-import static io.digdag.core.workflow.OperatorTestingUtils.newContext;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class MailOperatorFactoryTest
 {
 
-    private static Wiser wiser;
-
-    final static String hostName = "localhost";
-    final static int port = 2500;
+    private static GreenMail greenMail;
+    private static int smtpPort;
 
     private MailOperatorFactory factory;
     private Path tempPath;
-    private Config config;
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
@@ -42,16 +44,15 @@ public class MailOperatorFactoryTest
     @BeforeClass
     public static void smtpSetup()
     {
-        wiser = new Wiser();
-        wiser.setPort(port);
-        wiser.setHostname(hostName);
-        wiser.start();
+        greenMail = new GreenMail(ServerSetupTest.SMTP);
+        greenMail.start();
+        smtpPort = greenMail.getSmtp().getPort();
     }
 
     @AfterClass
     public static void shutdownSmtp()
     {
-        wiser.stop();
+        greenMail.stop();
     }
 
     @Before
@@ -64,30 +65,51 @@ public class MailOperatorFactoryTest
         Files.write(Paths.get(this.tempPath.toString(),"mail_body.txt"),body.getBytes());
     }
 
+    @After
+    public void resetGreenMail()
+    {
+        greenMail.reset();
+    }
+
     @Test
     public void sendEmail()
     {
         Config config = newConfig()
                 .set("host","localhost")
-                .set("port",2500)
+                .set("port",smtpPort)
                 .set("subject","test")
                 .set("_command","mail_body.txt")
                 .set("timezone","Asia/Tokyo")
                 .set("to","bob@example.com")
                 .set("from","alice@example.com");
-        try {
-            String hoge = Resources.toString(getClass().getResource("mail_body.txt"),UTF_8);
-            System.out.println(hoge);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        System.out.println(tempPath);
         Operator op = factory.newOperator(newContext(
                 tempPath,
                 newTaskRequest().withConfig(config)));
         op.run();
-        assertEquals(true,false);
+            assertEquals(1,greenMail.getReceivedMessages().length);
     }
+
+    @Test
+    public void sendEmail2()
+    {
+        Config config = newConfig()
+                .set("host","localhost")
+                .set("port",smtpPort)
+                .set("subject","test")
+                .set("_command","mail_body.txt")
+                .set("timezone","Asia/Tokyo")
+                .set("to","bob@example.com")
+                .set("from","alice@example.com")
+                .set("bcc", ImmutableList.of("charlie@example.com"))
+                .set("cc",ImmutableList.of("david@example.com"));
+
+        Operator op = factory.newOperator(newContext(
+                tempPath,
+                newTaskRequest().withConfig(config)));
+        op.run();
+        greenMail.waitForIncomingEmail(5000,3);
+//        assertEquals(3,greenMail.getReceivedMessages().length);
+    }
+
 }
