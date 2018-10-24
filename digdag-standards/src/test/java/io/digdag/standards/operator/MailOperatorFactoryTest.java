eaaeaff9865e1ae9d742b2fea2f541674935e995
Hiroyuki Sato <hiroysato@gmail.com>
2018-10-24 20:41:18 +0900

    Otimize imports.

--- digdag-standards/src/test/java/io/digdag/standards/operator/MailOperatorFactoryTest.java
+++ digdag-standards/src/test/java/io/digdag/standards/operator/MailOperatorFactoryTest.java
@@ -15,7 +15,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
