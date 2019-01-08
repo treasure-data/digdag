102c995d58a0e38a91587350c5a0ea21e7466b8f
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-01-08 12:26:02 -0800

    ac.AttemptTarget: Refactor several fields

--- digdag-spi/src/main/java/io/digdag/spi/ac/AttemptTarget.java
+++ digdag-spi/src/main/java/io/digdag/spi/ac/AttemptTarget.java
@@ -1,5 +1,6 @@
 package io.digdag.spi.ac;
 
+import com.google.common.base.Optional;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -8,39 +9,27 @@ public interface AttemptTarget
 {
     int getSiteId();
 
-    String getName();
+    Optional<String> getName();
 
     int getProjectId();
 
     String getProjectName();
 
-    long getWorkflowId();
-
     String getWorkflowName();
-
-    int getRevisionId();
-
-    String getRevisionName();
 
     static AttemptTarget of(
             int siteId,
-            String name,
+            Optional<String> name,
             int projectId,
             String projectName,
-            long workflowId,
-            String workflowName,
-            int revisionId,
-            String revisionName)
+            String workflowName)
     {
         return ImmutableAttemptTarget.builder()
                 .siteId(siteId)
                 .name(name)
                 .projectId(projectId)
                 .projectName(projectName)
-                .workflowId(workflowId)
                 .workflowName(workflowName)
-                .revisionId(revisionId)
-                .revisionName(revisionName)
                 .build();
     }
 }
