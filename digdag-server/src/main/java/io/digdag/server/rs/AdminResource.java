ae8881cc775f2e4c67159e0dbbc6822d70315c4c
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-01-09 14:52:35 -0800

    Revert "Refactor and fix AdminResource to apply access controller"
    This reverts commit af536a42d9b89c3e8eca80876c8c7c0437ba78f6.

--- digdag-server/src/main/java/io/digdag/server/rs/AdminResource.java
+++ digdag-server/src/main/java/io/digdag/server/rs/AdminResource.java
@@ -1,20 +1,13 @@
 package io.digdag.server.rs;
 
-import com.google.common.base.Optional;
 import com.google.inject.Inject;
 import io.digdag.client.config.Config;
 import io.digdag.core.database.TransactionManager;
-import io.digdag.core.repository.ProjectStore;
 import io.digdag.core.repository.ProjectStoreManager;
 import io.digdag.core.repository.ResourceNotFoundException;
-import io.digdag.core.repository.StoredProject;
 import io.digdag.core.repository.StoredRevision;
 import io.digdag.core.session.SessionStoreManager;
 import io.digdag.core.session.StoredSessionAttemptWithSession;
-import io.digdag.spi.ac.AccessController;
-import io.digdag.spi.ac.SessionTarget;
-import io.digdag.spi.ac.SiteTarget;
-import io.digdag.spi.ac.WorkflowTarget;
 import io.swagger.annotations.Api;
 
 import javax.ws.rs.GET;
@@ -34,19 +27,16 @@ public class AdminResource
     private final ProjectStoreManager pm;
     private final SessionStoreManager sm;
     private final TransactionManager tm;
-    private final AccessController ac;
 
     @Inject
     public AdminResource(
             ProjectStoreManager pm,
             SessionStoreManager sm,
-            TransactionManager tm,
-            AccessController ac)
+            TransactionManager tm)
     {
         this.pm = pm;
         this.sm = sm;
         this.tm = tm;
-        this.ac = ac;
     }
 
     @GET
@@ -55,35 +45,14 @@ public class AdminResource
             throws ResourceNotFoundException
     {
         return tm.begin(() -> {
-            StoredSessionAttemptWithSession session = sm.getAttemptWithSessionById(id, // NotFound
-                    () -> ac.getGetUserInfoFilterOf(
-                            SiteTarget.of(getSiteId()),
-                            getUserInfo())
-            );
-
+            StoredSessionAttemptWithSession session = sm.getAttemptWithSessionById(id);
 
             if (!session.getWorkflowDefinitionId().isPresent()) {
                 // TODO: is 404 appropriate in this situation?
                 throw new NotFoundException();
             }
 
-            final ProjectStore rs = pm.getProjectStore(getSiteId());
-            final StoredProject proj = rs.getProjectById(session.getSession().getProjectId(), // NotFound
-                    () -> ac.getGetUserInfoFilterOf( // TODO need to revisit to decide that we can use getGetProjectFilter method
-                            SiteTarget.of(getSiteId()),
-                            getUserInfo())
-            );
-
-            StoredRevision revision = pm.getRevisionOfWorkflowDefinition(session.getWorkflowDefinitionId().get(), // NotFound
-                    () -> {
-                        final WorkflowTarget wfTarget = WorkflowTarget.of(getSiteId(),
-                                session.getSession().getWorkflowName(),
-                                Optional.of(proj.getId()),
-                                proj.getName());
-                        return ac.getGetUserInfoFilterOf(wfTarget, getUserInfo()); // TODO need to revisit to decide that we can use getGetProjectFilter method
-                    }
-            );
-
+            StoredRevision revision = pm.getRevisionOfWorkflowDefinition(session.getWorkflowDefinitionId().get());
 
             return revision.getUserInfo();
         }, ResourceNotFoundException.class);
