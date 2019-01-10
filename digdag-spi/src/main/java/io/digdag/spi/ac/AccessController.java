7e736e3528275fa9c7f7e465ac15fef6d4787176
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-01-09 19:19:11 -0800

    Refactor and fix spi.ac.AccessController to introduce 403 throwing

--- digdag-spi/src/main/java/io/digdag/spi/ac/AccessController.java
+++ digdag-spi/src/main/java/io/digdag/spi/ac/AccessController.java
@@ -4,70 +4,211 @@ import io.digdag.client.config.Config;
 
 public interface AccessController
 {
-
     interface ListFilter
     {
         String getSql();
     }
 
-    // for project resource
-    // for workflow resource
-    // for session resource
+    // Projects
+
+    default void checkGetProjectOfProject(ProjectTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default ListFilter getListProjectsFilterOfSite(SiteTarget target, Config user)
+    {
+        return () -> "true";
+    }
+
+    default void checkPutProjectOfProject(ProjectTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkPutProjectOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkDeleteProjectOfProject(ProjectTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    // Workflows
+
+    default ListFilter getListWorkflowsFilterOfSite(SiteTarget target, Config user)
+    {
+        return () -> "true";
+    }
+
+    default ListFilter getListWorkflowsFilterOfProject(ProjectTarget target, Config user)
+    {
+        return () -> "true";
+    }
+
+    default void checkGetWorkflowOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkListWorkflowsOfProject(ProjectTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkListWorkflowsOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    // Sessions
+
+    /**
+     * Return a filter to check if the user has permissions to list sessions.
+     *
+     * @param target
+     * @param user
+     * @return
+     */
+    default ListFilter getListSessionsFilterOfProject(ProjectTarget target, Config user)
+    {
+        return () -> "true";
+    }
+
+    default ListFilter getListSessionsFilterOfWorkflow(WorkflowTarget target, Config user)
+    {
+        return () -> "true";
+    }
+
+    default ListFilter getListSessionsFilterOfSite(SiteTarget target, Config user)
+    {
+        return () -> "true";
+    }
+
+    default void checkListSessionsOfProject(ProjectTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkListSessionsOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkGetSessionOfSession(SessionTarget target, Config user)
+            throws AccessControlException
+    { }
+
     // for version resource
-    // for log resource
-    // for admin resource
-    // for ui resource
 
-    // for attempt resource
-    default void checkGetAttempt(Config userInfo, AttemptTarget target)
+    /** not support any filters for version resource */
+
+    // Log
+
+    default void checkListLogFilesOfAttempt(AttemptTarget target, Config user)
             throws AccessControlException
     { }
 
-    default void checkKillAttempt(Config userInfo, AttemptTarget target)
+    default void checkGetLogFileOfAttempt(AttemptTarget target, Config user)
             throws AccessControlException
     { }
 
-    default void checkListAttemptsOfProject(Config userInfo, ProjectTarget target)
+    default void checkPutLogFilesOfAttempt(AttemptTarget target, Config user)
             throws AccessControlException
     { }
 
-    default void checkListAttemptsOfWorkflow(Config userInfo, WorkflowTarget target)
+    // Admins
+
+    // Uis
+
+    // Attempts
+
+    default void checkListAttemptsOfProject(ProjectTarget target, Config user)
             throws AccessControlException
     { }
 
-    default void checkListAttempsOfSite(Config userInfo, SiteTarget target)
+    default void checkListAttemptsOfSession(SessionTarget target, Config user)
             throws AccessControlException
     { }
 
-    default void checkRunAttempt(Config userInfo, AttemptTarget target)
+    default void checkListAttemptsOfWorkflow(WorkflowTarget target, Config user)
             throws AccessControlException
     { }
 
-    default ListFilter getListAttemptsFilterOfProject(Config userInfo, ProjectTarget target)
+    default void checkListAttemptsOfAttempt(AttemptTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkListAttemptsOfSite(SiteTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default ListFilter getListAttemptsFilterOfProject(ProjectTarget target, Config user)
     {
         return () -> "true";
     }
 
-    default ListFilter getListAttemptsFilterOfWorkflow(Config userInfo, WorkflowTarget target)
+    default ListFilter getListAttemptsFilterOfWorkflow(WorkflowTarget target, Config user)
     {
         return () -> "true";
     }
 
-    default ListFilter getListAttemptsFilterOfSite(Config userInfo, SiteTarget target)
+    /**
+     * Return a filter to check if the user has permissions to list attempts.
+     *
+     * @param target
+     * @param user
+     * @return
+     */
+    default ListFilter getListAttemptsFilterOfSite(SiteTarget target, Config user)
     {
         return () -> "true";
     }
 
-    default ListFilter getGetAttemptFilterOfSite(Config userInfo, SiteTarget target)
+    default void checkGetAttemptOfAttempt(AttemptTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    /**
+     * Check if the user has permissions to run the attempt.
+     *
+     * @param target
+     * @param user
+     * @throws AccessControlException
+     */
+    default void checkRunAttemptOfAttempt(AttemptTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkKillAttemptOfAttempt(AttemptTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    // Schedules
+
+    default void checkGetScheduleOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkListSchedulesOfProject(ProjectTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkListSchedulesOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkSkipScheduleOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkBackfillScheduleOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkDisableScheduleOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default void checkEnableScheduleOfWorkflow(WorkflowTarget target, Config user)
+            throws AccessControlException
+    { }
+
+    default ListFilter getListSchedulesFilterOfSite(SiteTarget target, Config user)
     {
         return () -> "true";
     }
-
-    default ListFilter getGetWorkflowFilterOfSite(Config userInfo, SiteTarget target)
-    {
-        return () -> "true";
-    }
-
-    // for authenticated resource
-    // for schedule resource
 }
