12bbabce3e33d72e422757cab507219cc7ce40a6
You Yamagata <youy@bg8.so-net.ne.jp>
2019-01-28 13:54:04 +0900

    Add tests for retry with call, loop.

--- digdag-core/src/test/java/io/digdag/core/workflow/WorkflowExecutorTest.java
+++ digdag-core/src/test/java/io/digdag/core/workflow/WorkflowExecutorTest.java
@@ -127,6 +127,37 @@ public class WorkflowExecutorTest
 
 
     @Test
+    public void retryInCall()
+            throws Exception
+    {
+        String childContent = Resources.toString(WorkflowExecutorTest.class.getResource("/io/digdag/core/workflow/retry_in_call_child.dig"), UTF_8);
+        Path childPath = folder.getRoot().toPath().resolve("retry_in_call_child.dig");
+        Files.write(childPath, childContent.getBytes(UTF_8));
+
+        String parentBase = Resources.toString(WorkflowExecutorTest.class.getResource("/io/digdag/core/workflow/retry_in_call_parent.dig"), UTF_8);
+        String parentContent = parentBase.replaceFirst("child_path: dummy", "child_path: " + childPath.toString());
+
+        Config parent = new YamlConfigLoader().loadString(parentContent).toConfig(ConfigUtils.configFactory);
+        runWorkflow("retry_in_call", parent);
+        Path outPath = folder.getRoot().toPath().resolve("out");
+        assertThat(new String(Files.readAllBytes(outPath), UTF_8), is("try1try2try1try2try1try2"));
+    }
+
+
+    @Test
+    public void retryInLoop()
+            throws Exception
+    {
+        String content = Resources.toString(WorkflowExecutorTest.class.getResource("/io/digdag/core/workflow/retry_in_loop.dig"), UTF_8);
+        Config config = new YamlConfigLoader().loadString(content).toConfig(ConfigUtils.configFactory);
+        runWorkflow("retry_in_loop", config);
+        Path outPath = folder.getRoot().toPath().resolve("out");
+        String out = new String(Files.readAllBytes(outPath), UTF_8);
+        assertThat(new String(Files.readAllBytes(outPath), UTF_8), is("loop0:try1try2succeeded.loop1:try1try2succeeded.loop2:try1try2try1try2try1try2"));
+    }
+
+
+    @Test
     public void ifOperatorDelayedEvalDo()
             throws Exception
     {
