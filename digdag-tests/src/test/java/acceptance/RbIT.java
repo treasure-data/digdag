55040e01dcbcac436e359d356185bf4de0ef9866
sonots <sonots@gmail.com>
2019-02-26 22:23:06 +0900

    Added ruby option tests to RbIt

--- digdag-tests/src/test/java/acceptance/RbIT.java
+++ digdag-tests/src/test/java/acceptance/RbIT.java
@@ -49,7 +49,7 @@ public class RbIT
             throws Exception
     {
         final Path tempdir = folder.getRoot().toPath().toAbsolutePath();
-        final Path projectDir = tempdir.resolve("echo_params");
+        final Path projectDir = tempdir.resolve("rb");
         final Path scriptsDir = projectDir.resolve("scripts");
 
         // Create new project
@@ -64,7 +64,7 @@ public class RbIT
         // Push the project
         final CommandStatus pushStatus = main("push",
                 "--project", projectDir.toString(),
-                "echo_params",
+                "rb",
                 "-c", config.toString(),
                 "-e", server.endpoint(),
                 "-r", "4711");
@@ -74,7 +74,7 @@ public class RbIT
         final CommandStatus startStatus = main("start",
                 "-c", config.toString(),
                 "-e", server.endpoint(),
-                "echo_params", "echo_rb_params",
+                "rb", "echo_rb_params",
                 "--session", "now");
         assertThat(startStatus.code(), is(0));
         final Id attemptId = getAttemptId(startStatus);
@@ -92,5 +92,56 @@ public class RbIT
 
         final String logs = getAttemptLogs(client, attemptId);
         assertThat(logs, containsString("digdag params"));
+    }
+
+    @Test
+    public void verifyConfigurationRubyOption()
+            throws Exception
+    {
+        final Path tempdir = folder.getRoot().toPath().toAbsolutePath();
+        final Path projectDir = tempdir.resolve("rb");
+        final Path scriptsDir = projectDir.resolve("scripts");
+
+        // Create new project
+        final CommandStatus initStatus = main("init",
+                "-c", config.toString(),
+                projectDir.toString());
+        assertThat(initStatus.code(), is(0));
+        Files.createDirectories(scriptsDir);
+        copyResource("acceptance/rb/config_ruby.dig", projectDir.resolve("config_ruby.dig"));
+        copyResource("acceptance/echo_params/scripts/echo_params.rb", scriptsDir.resolve("echo_params.rb"));
+
+        // Push the project
+        final CommandStatus pushStatus = main("push",
+                "--project", projectDir.toString(),
+                "rb",
+                "-c", config.toString(),
+                "-e", server.endpoint(),
+                "-r", "4711");
+        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
+
+        // Start the workflow
+        final CommandStatus startStatus = main("start",
+                "-c", config.toString(),
+                "-e", server.endpoint(),
+                "rb", "config_ruby",
+                "--session", "now");
+        assertThat(startStatus.code(), is(0));
+        final Id attemptId = getAttemptId(startStatus);
+
+        // Wait for the attempt to complete
+        RestSessionAttempt attempt = null;
+        for (int i = 0; i < 30; i++) {
+            attempt = client.getSessionAttempt(attemptId);
+            if (attempt.getDone()) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        assertThat(attempt.getSuccess(), is(true));
+
+        final String logs = getAttemptLogs(client, attemptId);
+        assertThat(logs, containsString("ruby ruby"));
+        assertThat(logs, containsString("ruby [\"ruby\", \"-w\"]"));
     }
 }
