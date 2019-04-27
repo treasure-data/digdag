5c033f8c6948af5b011863f115cec862b4584e57
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-04-26 17:53:17 -0700

    AccessController: Update parameters of begin,autoCommit methods in digdag-core's database.TransactionManager

--- digdag-core/src/main/java/io/digdag/core/database/TransactionManager.java
+++ digdag-core/src/main/java/io/digdag/core/database/TransactionManager.java
@@ -12,53 +12,60 @@ public interface TransactionManager
     /**
      * Create a new transaction and set it as the current transaction object.
      */
-    <T> T begin(SupplierInTransaction<T, RuntimeException, RuntimeException, RuntimeException> func);
+    <T> T begin(SupplierInTransaction<T, RuntimeException, RuntimeException, RuntimeException, RuntimeException> func);
 
     /**
      * Create a new transaction and set it as the current transaction object.
      */
     <T, E1 extends Exception> T begin(
-            SupplierInTransaction<T, E1, RuntimeException, RuntimeException> func, Class<E1> e1)
+            SupplierInTransaction<T, E1, RuntimeException, RuntimeException, RuntimeException> func, Class<E1> e1)
         throws E1;
 
     /**
      * Create a new transaction and set it as the current transaction object.
      */
     <T, E1 extends Exception, E2 extends Exception> T begin(
-            SupplierInTransaction<T, E1, E2, RuntimeException> func, Class<E1> e1, Class<E2> e2)
+            SupplierInTransaction<T, E1, E2, RuntimeException, RuntimeException> func, Class<E1> e1, Class<E2> e2)
         throws E1, E2;
 
     /**
      * Create a new transaction and set it as the current transaction object.
      */
     <T, E1 extends Exception, E2 extends Exception, E3 extends Exception> T begin(
-            SupplierInTransaction<T, E1, E2, E3> func, Class<E1> e1, Class<E2> e2, Class<E3> e3)
+            SupplierInTransaction<T, E1, E2, E3, RuntimeException> func, Class<E1> e1, Class<E2> e2, Class<E3> e3)
         throws E1, E2, E3;
+
+    /**
+     * Create a new transaction and set it as the current transaction object.
+     */
+    <T, E1 extends Exception, E2 extends Exception, E3 extends Exception, E4 extends Exception> T begin(
+            SupplierInTransaction<T, E1, E2, E3, E4> func, Class<E1> e1, Class<E2> e2, Class<E3> e3, Class<E4> e4)
+            throws E1, E2, E3, E4;
 
     /**
      * Get the current transaction object if exists, otherwise uses a temporary transaction object with auto-commit mode.
      */
-    <T> T autoCommit(SupplierInTransaction<T, RuntimeException, RuntimeException, RuntimeException> func);
+    <T> T autoCommit(SupplierInTransaction<T, RuntimeException, RuntimeException, RuntimeException, RuntimeException> func);
 
     /**
      * Get the current transaction object if exists, otherwise uses a temporary transaction object with auto-commit mode.
      */
     <T, E1 extends Exception> T autoCommit(
-            SupplierInTransaction<T, E1, RuntimeException, RuntimeException> func, Class<E1> e1)
+            SupplierInTransaction<T, E1, RuntimeException, RuntimeException, RuntimeException> func, Class<E1> e1)
         throws E1;
 
     /**
      * Get the current transaction object if exists, otherwise uses a temporary transaction object with auto-commit mode.
      */
     <T, E1 extends Exception, E2 extends Exception> T autoCommit(
-            SupplierInTransaction<T, E1, E2, RuntimeException> func, Class<E1> e1, Class<E2> e2)
+            SupplierInTransaction<T, E1, E2, RuntimeException, RuntimeException> func, Class<E1> e1, Class<E2> e2)
         throws E1, E2;
 
     /**
      * Get the current transaction object if exists, otherwise uses a temporary transaction object with auto-commit mode.
      */
     <T, E1 extends Exception, E2 extends Exception, E3 extends Exception> T autoCommit(
-            SupplierInTransaction<T, E1, E2, E3> func, Class<E1> e1, Class<E2> e2, Class<E3> e3)
+            SupplierInTransaction<T, E1, E2, E3, RuntimeException> func, Class<E1> e1, Class<E2> e2, Class<E3> e3)
         throws E1, E2, E3;
 
     /**
@@ -67,8 +74,9 @@ public interface TransactionManager
     void reset();
 
     @FunctionalInterface
-    interface SupplierInTransaction<T, E1 extends Exception, E2 extends Exception, E3 extends Exception>
+    interface SupplierInTransaction<T, E1 extends Exception, E2 extends Exception, E3 extends Exception, E4 extends Exception>
     {
-        T get() throws E1, E2, E3;
+        T get()
+                throws E1, E2, E3, E4;
     }
 }
