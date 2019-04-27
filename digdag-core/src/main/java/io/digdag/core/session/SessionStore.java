ad2dd82cb0c4211ee553fe4e566cd98f13ef9050
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-04-26 17:48:04 -0700

    AccessController: Update digdag-core's SessionStore

--- digdag-core/src/main/java/io/digdag/core/session/SessionStore.java
+++ digdag-core/src/main/java/io/digdag/core/session/SessionStore.java
@@ -3,26 +3,26 @@ package io.digdag.core.session;
 import java.util.List;
 import java.time.Instant;
 import com.google.common.base.Optional;
-import io.digdag.core.repository.ResourceConflictException;
 import io.digdag.core.repository.ResourceNotFoundException;
+import io.digdag.spi.ac.AccessController;
 
 public interface SessionStore
         extends SessionTransaction
 {
-    List<StoredSessionWithLastAttempt> getSessions(int pageSize, Optional<Long> lastId);
+    List<StoredSessionWithLastAttempt> getSessions(int pageSize, Optional<Long> lastId, AccessController.ListFilter acFilter);
 
     StoredSessionWithLastAttempt getSessionById(long sessionId)
         throws ResourceNotFoundException;
 
-    List<StoredSessionWithLastAttempt> getSessionsOfProject(int projectId, int pageSize, Optional<Long> lastId);
+    List<StoredSessionWithLastAttempt> getSessionsOfProject(int projectId, int pageSize, Optional<Long> lastId, AccessController.ListFilter acFilter);
 
-    List<StoredSessionWithLastAttempt> getSessionsOfWorkflowByName(int projectId, String workflowName, int pageSize, Optional<Long> lastId);
+    List<StoredSessionWithLastAttempt> getSessionsOfWorkflowByName(int projectId, String workflowName, int pageSize, Optional<Long> lastId, AccessController.ListFilter acFilter);
 
-    List<StoredSessionAttemptWithSession> getAttempts(boolean withRetriedAttempts, int pageSize, Optional<Long> lastId);
+    List<StoredSessionAttemptWithSession> getAttempts(boolean withRetriedAttempts, int pageSize, Optional<Long> lastId, AccessController.ListFilter acFilter);
 
-    List<StoredSessionAttemptWithSession> getAttemptsOfProject(boolean withRetriedAttempts, int projectId, int pageSize, Optional<Long> lastId);
+    List<StoredSessionAttemptWithSession> getAttemptsOfProject(boolean withRetriedAttempts, int projectId, int pageSize, Optional<Long> lastId, AccessController.ListFilter acFilter);
 
-    List<StoredSessionAttemptWithSession> getAttemptsOfWorkflow(boolean withRetriedAttempts, int projectId, String workflowName, int pageSize, Optional<Long> lastId);
+    List<StoredSessionAttemptWithSession> getAttemptsOfWorkflow(boolean withRetriedAttempts, int projectId, String workflowName, int pageSize, Optional<Long> lastId, AccessController.ListFilter acFilter);
 
     List<StoredSessionAttemptWithSession> getActiveAttemptsOfWorkflow(int projectId, String workflowName, int pageSize, Optional<Long> lastId);
 
