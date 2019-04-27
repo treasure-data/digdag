cdc4f48a44352decfc4614b23dce6f816e05a5ab
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-04-26 21:50:24 -0700

    KubernetesCommandExecutor: bind KubernetesCommandExecutor as first CommandExecutor on CommandExecutorModule

--- digdag-standards/src/main/java/io/digdag/standards/command/CommandExecutorModule.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/CommandExecutorModule.java
@@ -4,6 +4,8 @@ import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
 import io.digdag.spi.CommandExecutor;
+import io.digdag.standards.command.kubernetes.DefaultKubernetesClientFactory;
+import io.digdag.standards.command.kubernetes.KubernetesClientFactory;
 
 public class CommandExecutorModule
     implements Module
@@ -11,7 +13,9 @@ public class CommandExecutorModule
     @Override
     public void configure(Binder binder)
     {
-        binder.bind(CommandExecutor.class).to(DockerCommandExecutor.class).in(Scopes.SINGLETON);
+        binder.bind(CommandExecutor.class).to(KubernetesCommandExecutor.class).in(Scopes.SINGLETON);
+        binder.bind(KubernetesClientFactory.class).to(DefaultKubernetesClientFactory.class).in(Scopes.SINGLETON);
         binder.bind(SimpleCommandExecutor.class).in(Scopes.SINGLETON);
+        binder.bind(DockerCommandExecutor.class).in(Scopes.SINGLETON);
     }
 }
