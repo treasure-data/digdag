143f954fdad17cdbe77c88a4732ca9bbd667229e
You Yamagata <youy@bg8.so-net.ne.jp>
2019-05-24 16:16:14 -0700

    Remove unnecessary comment.

--- docker/bootstrap/dependencies.sh
+++ docker/bootstrap/dependencies.sh
@@ -30,8 +30,6 @@ sudo -u postgres createdb -O digdag_test digdag_test
 # Python
 apt-get -y install python python-pip python-dev
 pip install pip --upgrade && hash -r pip # rehashed https://github.com/pypa/pip/issues/5240
-# Using sphinx==1.4.9 because sphinx_rtd_theme with sphinx 1.5.x has a problem with search and its fix is not released: https://github.com/snide/sphinx_rtd_theme/pull/346
-# Update above PR has been already merged.
 pip install sphinx==1.5.6 recommonmark sphinx_rtd_theme
 
 # Ruby
