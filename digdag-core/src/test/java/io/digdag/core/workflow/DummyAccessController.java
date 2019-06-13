4f82a02bb8a197b67fbc39545193697fb327316c
You Yamagata <youy@bg8.so-net.ne.jp>
2019-06-13 14:46:30 +0900

    Fix cross module dependencies.

---
+++ digdag-core/src/test/java/io/digdag/core/workflow/DummyAccessController.java
@@ -0,0 +1,213 @@
+package io.digdag.core.workflow;
+
+import io.digdag.spi.AuthenticatedUser;
+import io.digdag.spi.ac.AccessControlException;
+import io.digdag.spi.ac.AccessController;
+import io.digdag.spi.ac.AttemptTarget;
+import io.digdag.spi.ac.ProjectTarget;
+import io.digdag.spi.ac.ScheduleTarget;
+import io.digdag.spi.ac.SecretTarget;
+import io.digdag.spi.ac.SiteTarget;
+import io.digdag.spi.ac.WorkflowTarget;
+
+public class DummyAccessController
+        implements AccessController
+{
+    @Override
+    public void checkPutProject(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkDeleteProject(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetProject(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkListProjectsOfSite(SiteTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListProjectsFilterOfSite(SiteTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkGetProjectArchive(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkPutProjectSecret(SecretTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkDeleteProjectSecret(SecretTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetProjectSecretList(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetWorkflow(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkListWorkflowsOfSite(SiteTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListWorkflowsFilterOfSite(SiteTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkListWorkflowsOfProject(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListWorkflowsFilterOfProject(ProjectTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkRunWorkflow(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkPutLogFile(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetLogFiles(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetSession(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkListSessionsOfSite(SiteTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListSessionsFilterOfSite(SiteTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkListSessionsOfProject(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListSessionsFilterOfProject(ProjectTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkListSessionsOfWorkflow(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListSessionsFilterOfWorkflow(WorkflowTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkGetAttemptsFromSession(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetAttempt(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetTasksFromAttempt(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkKillAttempt(AttemptTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkGetSchedule(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkListSchedulesOfSite(SiteTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListSchedulesFilterOfSite(SiteTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkListSchedulesOfProject(ProjectTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public ListFilter getListSchedulesFilterOfProject(ProjectTarget target, AuthenticatedUser user)
+    {
+        return () -> "true";
+    }
+
+    @Override
+    public void checkGetScheduleFromWorkflow(WorkflowTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkSkipSchedule(ScheduleTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkBackfillSchedule(ScheduleTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkDisableSchedule(ScheduleTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+
+    @Override
+    public void checkEnableSchedule(ScheduleTarget target, AuthenticatedUser user)
+            throws AccessControlException
+    { }
+}
