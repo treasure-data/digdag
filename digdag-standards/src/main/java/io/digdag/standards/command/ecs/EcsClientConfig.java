435208576443ecda855e3a9e88c3a0ef7812614b
Edd Steel <edward@treasure-data.com>
2019-07-10 08:51:40 -0700

    Add max-retries option for ECS.

--- digdag-standards/src/main/java/io/digdag/standards/command/ecs/EcsClientConfig.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/ecs/EcsClientConfig.java
@@ -52,7 +52,8 @@ public class EcsClientConfig
                 extracted.get("access_key_id", String.class),
                 extracted.get("secret_access_key", String.class),
                 extracted.get("region", String.class),
-                extracted.get("subnets", String.class)
+                extracted.get("subnets", String.class),
+                extracted.get("max_retries", Integer.class)
         );
     }
 
@@ -62,13 +63,15 @@ public class EcsClientConfig
     private final String secretAccessKey;
     private final String region;
     private final List<String> subnets;
+    private final Integer maxRetries;
 
     private EcsClientConfig(final String clusterName,
             final String launchType,
             final String accessKeyId,
             final String secretAccessKey,
             final String region,
-            final String subnets)
+            final String subnets,
+            final Integer maxRetries)
     {
         this.clusterName = clusterName;
         this.launchType = launchType;
@@ -76,6 +79,7 @@ public class EcsClientConfig
         this.secretAccessKey = secretAccessKey;
         this.region = region;
         this.subnets = Arrays.asList(subnets.split(",")); // TODO more robust
+        this.maxRetries = maxRetries;
     }
 
     public String getClusterName()
@@ -107,4 +111,6 @@ public class EcsClientConfig
     {
         return subnets;
     }
+
+    public Integer getMaxRetries() { return maxRetries; }
 }
