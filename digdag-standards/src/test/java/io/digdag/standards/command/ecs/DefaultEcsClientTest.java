bb5063ce79fe38aea1aec1ecdd327242d186d455
Muga Nishizawa <muga.nishizawa@gmail.com>
2019-07-13 00:10:00 -0700

    EcsCommandExecutor: Add getTaskDefinitionTags method in EcsClient

---
+++ digdag-standards/src/test/java/io/digdag/standards/command/ecs/DefaultEcsClientTest.java
@@ -0,0 +1,54 @@
+package io.digdag.standards.command.ecs;
+
+import com.amazonaws.services.ecs.AmazonECSClient;
+import com.amazonaws.services.ecs.model.ListTagsForResourceRequest;
+import com.amazonaws.services.ecs.model.ListTagsForResourceResult;
+import com.amazonaws.services.ecs.model.Tag;
+import com.amazonaws.services.logs.AWSLogs;
+import com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DefaultEcsClientTest
+{
+    @Mock private EcsClientConfig ecsClientConfig;
+    @Mock private AmazonECSClient rawEcsClient;
+    @Mock private AWSLogs logs;
+
+    private DefaultEcsClient ecsClient;
+
+    @Before
+    public void setUp()
+    {
+        ecsClient = spy(new DefaultEcsClient(ecsClientConfig, rawEcsClient, logs));
+    }
+
+    @Test
+    public void testGetTaskDefinitionTags()
+            throws Exception
+    {
+        final List<Tag> expected = ImmutableList.of(
+                new Tag().withKey("k1").withValue("v1"),
+                new Tag().withKey("k2").withValue("v2")
+        );
+        final ListTagsForResourceResult result = new ListTagsForResourceResult()
+                .withTags(expected);
+        doReturn(result).when(rawEcsClient).listTagsForResource(any(ListTagsForResourceRequest.class));
+
+        List<Tag> tags = ecsClient.getTaskDefinitionTags("my_task_def_arn");
+        assertEquals(expected.size(), tags.size());
+        assertEquals(expected.get(0), tags.get(0));
+        assertEquals(expected.get(1), tags.get(1));
+    }
+}
