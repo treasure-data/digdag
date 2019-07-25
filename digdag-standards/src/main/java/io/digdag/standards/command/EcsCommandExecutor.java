a91fcf6ff525d2dc277cb1afb94973bfcf4e5423
You Yamagata <youy@bg8.so-net.ne.jp>
2019-07-25 16:14:54 +0900

    Change log level to DEBUG.

--- digdag-standards/src/main/java/io/digdag/standards/command/EcsCommandExecutor.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/EcsCommandExecutor.java
@@ -116,7 +116,7 @@ public class EcsCommandExecutor
             }
         }
         catch (ConfigException e) {
-            logger.trace("Fall back to DockerCommandExecutor: {}", e.toString());
+            logger.debug("Fall back to DockerCommandExecutor: {}", e.toString());
             return docker.run(commandContext, commandRequest); // fall back to DockerCommandExecutor
         }
     }
