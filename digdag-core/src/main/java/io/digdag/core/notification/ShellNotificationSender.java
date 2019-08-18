fb33678c0c978201dcf910b7e17909dce501ece4
katsuyan <zakichan1216@gmail.com>
2019-08-18 20:43:30 +0900

    output log ShellNotificationSender

--- digdag-core/src/main/java/io/digdag/core/notification/ShellNotificationSender.java
+++ digdag-core/src/main/java/io/digdag/core/notification/ShellNotificationSender.java
@@ -2,6 +2,7 @@ package io.digdag.core.notification;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
 import com.google.common.io.ByteStreams;
 import com.google.inject.Inject;
@@ -17,6 +18,8 @@ import java.io.OutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static java.lang.ProcessBuilder.Redirect;
+import static java.lang.ProcessBuilder.Redirect.INHERIT;
 import static java.lang.ProcessBuilder.Redirect.PIPE;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
@@ -25,11 +28,13 @@ public class ShellNotificationSender
 {
     private static final String NOTIFICATION_SHELL_COMMAND = "notification.shell.command";
     private static final String NOTIFICATION_SHELL_TIMEOUT = "notification.shell.timeout";
+    private static final String NOTIFICATION_SHELL_LOGFILE = "notification.shell.logfile";
     private static final int NOTIFICATION_SHELL_TIMEOUT_DEFAULT = 30_000;
 
     private final String command;
     private final ObjectMapper mapper;
     private final int timeoutMs;
+    private Optional<String> logFileName;
 
     @Inject
     public ShellNotificationSender(Config systemConfig, ObjectMapper mapper)
@@ -37,6 +42,7 @@ public class ShellNotificationSender
         this.command = systemConfig.get(NOTIFICATION_SHELL_COMMAND, String.class);
         this.mapper = mapper;
         this.timeoutMs = systemConfig.get(NOTIFICATION_SHELL_TIMEOUT, int.class, NOTIFICATION_SHELL_TIMEOUT_DEFAULT);
+        this.logFileName = Optional.fromNullable(systemConfig.get(NOTIFICATION_SHELL_LOGFILE, String.class, null));
     }
 
     @Override
@@ -53,11 +59,16 @@ public class ShellNotificationSender
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        File devNull = new File("/dev/null");
+        Redirect redirectLog;
+        if (logFileName.isPresent()) {
+            redirectLog = Redirect.to(new File(logFileName.get()));
+        } else {
+            redirectLog = INHERIT;
+        }
 
         ProcessBuilder processBuilder = new ProcessBuilder()
-                .redirectOutput(devNull)
-                .redirectError(devNull)
+                .redirectOutput(redirectLog)
+                .redirectError(redirectLog)
                 .redirectInput(PIPE)
                 .command("/bin/sh", "-c", command);
 
