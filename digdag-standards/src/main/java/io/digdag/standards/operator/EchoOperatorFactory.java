5687b0bb300f91cf1f2854e42a2d6c933a272859
You Yamagata <youy@bg8.so-net.ne.jp>
2019-09-12 17:02:02 +0900

    Add echo log output to task log.

--- digdag-standards/src/main/java/io/digdag/standards/operator/EchoOperatorFactory.java
+++ digdag-standards/src/main/java/io/digdag/standards/operator/EchoOperatorFactory.java
@@ -2,11 +2,15 @@ package io.digdag.standards.operator;
 
 import com.google.inject.Inject;
 import io.digdag.client.config.Config;
+import io.digdag.core.log.TaskContextLogging;
+import io.digdag.core.log.TaskLogger;
 import io.digdag.spi.Operator;
 import io.digdag.spi.OperatorContext;
 import io.digdag.spi.OperatorFactory;
 import io.digdag.spi.TaskRequest;
 import io.digdag.spi.TaskResult;
+
+import java.nio.charset.StandardCharsets;
 
 public class EchoOperatorFactory
         implements OperatorFactory
@@ -44,6 +48,11 @@ public class EchoOperatorFactory
             String message = params.get("_command", String.class);
 
             System.out.println(message);
+
+            // To store the message to task log
+            TaskLogger logger = TaskContextLogging.getContext().getLogger();
+            byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+            logger.log(messageBytes, 0, messageBytes.length);
 
             return TaskResult.empty(request);
         }
