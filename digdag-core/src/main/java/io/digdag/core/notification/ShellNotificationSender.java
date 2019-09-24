39185c5f06085958a3bd929d86f3146d9a836921
Katsuya Tajima <zakichan1216@gmail.com>
2019-09-24 22:51:26 +0900

    change output log ShellNotificationSendor to server log

--- digdag-core/src/main/java/io/digdag/core/notification/ShellNotificationSender.java
+++ digdag-core/src/main/java/io/digdag/core/notification/ShellNotificationSender.java
@@ -5,17 +5,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Throwables;
 import com.google.common.io.ByteStreams;
 import com.google.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.digdag.client.config.Config;
-import io.digdag.spi.CommandLogger;
 import io.digdag.spi.Notification;
 import io.digdag.spi.NotificationException;
 import io.digdag.spi.NotificationSender;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 import static java.lang.ProcessBuilder.Redirect.PIPE;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -23,21 +29,21 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class ShellNotificationSender
         implements NotificationSender
 {
+    private static final Logger logger = LoggerFactory.getLogger(ShellNotificationSender.class);
+
     private static final String NOTIFICATION_SHELL_COMMAND = "notification.shell.command";
     private static final String NOTIFICATION_SHELL_TIMEOUT = "notification.shell.timeout";
     private static final int NOTIFICATION_SHELL_TIMEOUT_DEFAULT = 30_000;
 
     private final String command;
     private final ObjectMapper mapper;
-    private final CommandLogger clog;
     private final int timeoutMs;
 
     @Inject
-    public ShellNotificationSender(Config systemConfig, CommandLogger clog, ObjectMapper mapper)
+    public ShellNotificationSender(Config systemConfig, ObjectMapper mapper)
     {
         this.command = systemConfig.get(NOTIFICATION_SHELL_COMMAND, String.class);
         this.mapper = mapper;
-        this.clog = clog;
         this.timeoutMs = systemConfig.get(NOTIFICATION_SHELL_TIMEOUT, int.class, NOTIFICATION_SHELL_TIMEOUT_DEFAULT);
     }
 
@@ -54,7 +60,6 @@ public class ShellNotificationSender
         }
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
-
 
         ProcessBuilder processBuilder = new ProcessBuilder()
                 .redirectErrorStream(true)
@@ -76,18 +81,28 @@ public class ShellNotificationSender
                 }
             });
 
-            // read stdout to stdout
-            clog.copyStdout(process, System.out);
-
             boolean exited = process.waitFor(timeoutMs, MILLISECONDS);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            Stream<String> lines = br.lines();
+            StringBuilder sb = new StringBuilder();
+            lines.forEach(line -> {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            });
+            String processLog = sb.toString();
+
             if (!exited) {
                 process.destroyForcibly();
-                throw new NotificationException("Notification shell command timed out: " + command);
+                throw new NotificationException("Notification shell command timed out: " + command + ", command log:" + System.lineSeparator() + processLog);
             }
             int exitCode = process.exitValue();
             if (exitCode != 0) {
-                throw new NotificationException("Notification shell command failed: " + command + ", exit code = " + exitCode);
+                throw new NotificationException("Notification shell command failed: " + command + ", exit code = " + exitCode + ", command log:" + System.lineSeparator() + processLog);
             }
+
+            logger.info("Notification shell command: " + command);
+            logger.info("Notification shell command log:" + System.lineSeparator() + processLog);
         }
         catch (IOException e) {
             throw new NotificationException("Failed to execute notification shell command: " + command, e);
