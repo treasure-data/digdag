478b22a1a0776cc420578ce88c4b26ca76a2d99a
Ritta Narita <narittan@gmail.com>
2019-10-18 17:13:04 +0900

    Fix TDOperator not to require database setting for result export

--- digdag-standards/src/main/java/io/digdag/standards/operator/td/TDOperator.java
+++ digdag-standards/src/main/java/io/digdag/standards/operator/td/TDOperator.java
@@ -53,9 +53,13 @@ public class TDOperator
 
     public static TDOperator fromConfig(SystemDefaultConfig systemDefaultConfig, Map<String, String> env, Config config, SecretProvider secrets)
     {
-        String database = secrets.getSecretOptional("database").or(config.get("database", String.class)).trim();
-        if (database.isEmpty()) {
-            throw new ConfigException("Parameter 'database' is empty");
+        String database = null;
+        String type = config.get("_type", String.class);
+        if (type != null && !type.equals("td_result_export")) {
+            database = secrets.getSecretOptional("database").or(config.get("database", String.class)).trim();
+            if (database.isEmpty()) {
+                throw new ConfigException("Parameter 'database' is empty");
+            }
         }
 
         TDClient client = TDClientFactory.clientFromConfig(systemDefaultConfig, env, config, secrets);
