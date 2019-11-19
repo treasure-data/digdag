f03cb632f8340e1336f9c505c9b0365c5964d15f
TrsNium <trsnium@gmail.com>
2019-11-19 19:51:37 +0900

    reformat code

--- digdag-standards/src/test/java/io/digdag/standards/command/kubernetes/KubernetesClientConfigTest.java
+++ digdag-standards/src/test/java/io/digdag/standards/command/kubernetes/KubernetesClientConfigTest.java
@@ -1,36 +1,20 @@
 package io.digdag.standards.command.kubernetes;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Optional;
 import io.digdag.client.config.Config;
 import io.digdag.client.config.ConfigFactory;
-import io.digdag.core.archive.ProjectArchiveLoader;
-import io.digdag.core.storage.StorageManager;
-import io.digdag.spi.CommandContext;
-import io.digdag.spi.CommandLogger;
-import io.digdag.spi.CommandRequest;
-import io.digdag.spi.CommandStatus;
-import io.digdag.spi.TaskRequest;
 import io.digdag.standards.command.kubernetes.KubernetesClientConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import java.lang.reflect.Method;
 
-import java.lang.ClassLoader;
 import java.io.File;
+import java.lang.reflect.Method;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(MockitoJUnitRunner.class)
 public class KubernetesClientConfigTest
