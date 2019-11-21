231575a42ad9c6e10a5f302fe0e7c5c1324ed577
Ritta Narita <narittan@gmail.com>
2019-11-21 16:03:16 +0900

    Add integration test

--- digdag-tests/src/test/java/acceptance/td/TdResultExportIT.java
+++ digdag-tests/src/test/java/acceptance/td/TdResultExportIT.java
@@ -3,6 +3,7 @@ package acceptance.td;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
 import com.treasuredata.client.TDApiRequest;
 import com.treasuredata.client.TDClient;
 import com.treasuredata.client.TDClientConfig;
@@ -11,6 +12,9 @@ import com.treasuredata.client.model.TDJob;
 import com.treasuredata.client.model.TDSaveQueryRequest;
 import com.treasuredata.client.model.TDSavedQuery;
 import com.treasuredata.client.model.TDSavedQueryStartRequest;
+import io.digdag.client.DigdagClient;
+import io.digdag.client.api.Id;
+import io.netty.handler.codec.http.FullHttpRequest;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -19,18 +23,34 @@ import okhttp3.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.littleshoot.proxy.HttpProxyServer;
+import utils.TemporaryDigdagServer;
+import utils.TestUtils;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import static acceptance.td.Secrets.TD_API_KEY;
 import static acceptance.td.Secrets.TD_API_ENDPOINT;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assume.assumeThat;
+import static utils.TestUtils.attemptSuccess;
+import static utils.TestUtils.copyResource;
+import static utils.TestUtils.expect;
+import static utils.TestUtils.pushAndStart;
+import static utils.TestUtils.startRequestTrackingProxy;
 
 public class TdResultExportIT
 {
@@ -42,6 +62,8 @@ public class TdResultExportIT
     private String connectionId;
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private OkHttpClient httpClient;
+    private Path projectDir;
+    private Path outfile;
 
     @Before
     public void setUp()
@@ -66,6 +88,44 @@ public class TdResultExportIT
         this.table = "test_" + UUID.randomUUID().toString().replace('-', '_');
         client.createTableIfNotExists(database, table);
         this.httpClient = new OkHttpClient();
+    }
+
+    @Test
+    public void testRunResultExport()
+            throws Exception
+    {
+        List<FullHttpRequest> requests = Collections.synchronizedList(new ArrayList<>());
+
+        HttpProxyServer proxyServer = startRequestTrackingProxy(requests);
+
+        String proxyUrl = "http://" + proxyServer.getListenAddress().getHostString() + ":" + proxyServer.getListenAddress().getPort();
+
+        TemporaryDigdagServer server = TemporaryDigdagServer.builder()
+                .configuration(Secrets.secretsServerConfiguration())
+                .environment(ImmutableMap.of("http_proxy", proxyUrl))
+                .build();
+
+        server.start();
+
+        copyResource("acceptance/td/td/td_result_export.dig", projectDir.resolve("td_result_export.dig"));
+        copyResource("acceptance/td/td/query.sql", projectDir.resolve("query.sql"));
+
+        Id projectId = TestUtils.pushProject(server.endpoint(), projectDir);
+
+        DigdagClient digdagClient = DigdagClient.builder()
+                .host(server.host())
+                .port(server.port())
+                .build();
+
+        digdagClient.setProjectSecret(projectId, "td.apikey", TD_API_KEY);
+
+        Id attemptId = pushAndStart(server.endpoint(), projectDir, "workflow", ImmutableMap.of(
+                "outfile", outfile.toString(),
+                "td.use_ssl", "false"));
+
+        expect(Duration.ofMinutes(5), attemptSuccess(server.endpoint(), attemptId));
+
+        assertThat(requests.stream().filter(req -> req.getUri().contains("/v3/job/issue")).count(), is(greaterThan(0L)));
     }
 
     @Test
