183c474c81845346c2660c37353ce699c0b7e269
Ritta Narita <narittan@gmail.com>
2019-11-29 12:33:50 +0900

    Fix integration test to use right proxy setting

--- digdag-tests/src/test/java/acceptance/td/TdResultExportIT.java
+++ digdag-tests/src/test/java/acceptance/td/TdResultExportIT.java
@@ -2,16 +2,16 @@ package acceptance.td;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Optional;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
-import com.treasuredata.client.TDApiRequest;
+import com.google.common.io.CharStreams;
 import com.treasuredata.client.TDClient;
-import com.treasuredata.client.TDClientConfig;
-import com.treasuredata.client.model.TDExportResultJobRequest;
 import com.treasuredata.client.model.TDJob;
-import com.treasuredata.client.model.TDSaveQueryRequest;
-import com.treasuredata.client.model.TDSavedQuery;
-import com.treasuredata.client.model.TDSavedQueryStartRequest;
+import com.treasuredata.client.model.TDJobRequest;
+import com.treasuredata.client.model.TDJobSummary;
+import com.treasuredata.client.model.TDResultFormat;
 import io.digdag.client.DigdagClient;
 import io.digdag.client.api.Id;
 import io.netty.handler.codec.http.FullHttpRequest;
@@ -22,48 +22,56 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.littleshoot.proxy.HttpProxyServer;
 import utils.TemporaryDigdagServer;
 import utils.TestUtils;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Callable;
 
 import static acceptance.td.Secrets.TD_API_KEY;
 import static acceptance.td.Secrets.TD_API_ENDPOINT;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeThat;
+import static utils.TestUtils.addWorkflow;
 import static utils.TestUtils.attemptSuccess;
 import static utils.TestUtils.copyResource;
 import static utils.TestUtils.expect;
+import static utils.TestUtils.objectMapper;
 import static utils.TestUtils.pushAndStart;
 import static utils.TestUtils.startRequestTrackingProxy;
 
 public class TdResultExportIT
 {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     private TDClient client;
     private String database;
     private String table;
-    private String savedQuery;
-    private String jobId;
     private String connectionId;
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private OkHttpClient httpClient;
     private Path projectDir;
-    private Path outfile;
+
+    public TemporaryDigdagServer server;
+
+    private HttpProxyServer proxyServer;
+    private String sampleJobId;
 
     @Before
     public void setUp()
@@ -71,69 +79,27 @@ public class TdResultExportIT
     {
         assumeThat(TD_API_KEY, not(isEmptyOrNullString()));
         assumeThat(TD_API_ENDPOINT, not(isEmptyOrNullString()));
+        projectDir = folder.getRoot().toPath().toAbsolutePath().normalize();
 
         client = TDClient.newBuilder(false)
                 .setApiKey(TD_API_KEY)
                 .setEndpoint(TD_API_ENDPOINT)
                 .build();
-        database = "tmp_" + UUID.randomUUID().toString().replace('-', '_');
-        client.createDatabase(database);
-        String queryName = "test_" + UUID.randomUUID().toString().replace('-', '_');
-        createQuery(queryName);
-        TDSavedQueryStartRequest req = TDSavedQueryStartRequest.builder()
-                .name(queryName)
-                .scheduledTime(new Date())
-                .build();
-        this.jobId = client.startSavedQuery(req);
-        this.table = "test_" + UUID.randomUUID().toString().replace('-', '_');
+        sampleJobId = client.submit(TDJobRequest.newPrestoQuery("sample_datasets",
+                "select time from www_access"));
+        database = "_digdag_integration_result_export_td_test_db";
+        client.createDatabaseIfNotExists(database);
+        table = "_digdag_integration_result_export_td_test_table";
         client.createTableIfNotExists(database, table);
         this.httpClient = new OkHttpClient();
     }
 
     @Test
-    public void testRunResultExport()
+    public void testSubmitResultExportJob()
             throws Exception
     {
-        List<FullHttpRequest> requests = Collections.synchronizedList(new ArrayList<>());
-
-        HttpProxyServer proxyServer = startRequestTrackingProxy(requests);
-
-        String proxyUrl = "http://" + proxyServer.getListenAddress().getHostString() + ":" + proxyServer.getListenAddress().getPort();
-
-        TemporaryDigdagServer server = TemporaryDigdagServer.builder()
-                .configuration(Secrets.secretsServerConfiguration())
-                .environment(ImmutableMap.of("http_proxy", proxyUrl))
-                .build();
-
-        server.start();
-
-        copyResource("acceptance/td/td/td_result_export.dig", projectDir.resolve("td_result_export.dig"));
-        copyResource("acceptance/td/td/query.sql", projectDir.resolve("query.sql"));
-
-        Id projectId = TestUtils.pushProject(server.endpoint(), projectDir);
-
-        DigdagClient digdagClient = DigdagClient.builder()
-                .host(server.host())
-                .port(server.port())
-                .build();
-
-        digdagClient.setProjectSecret(projectId, "td.apikey", TD_API_KEY);
-
-        Id attemptId = pushAndStart(server.endpoint(), projectDir, "workflow", ImmutableMap.of(
-                "outfile", outfile.toString(),
-                "td.use_ssl", "false"));
-
-        expect(Duration.ofMinutes(5), attemptSuccess(server.endpoint(), attemptId));
-
-        assertThat(requests.stream().filter(req -> req.getUri().contains("/v3/job/issue")).count(), is(greaterThan(0L)));
-    }
-
-    @Test
-    public void testSubmitResultExportJob()
-            throws IOException
-    {
-        String testConnectorName =  "test_" + UUID.randomUUID().toString().replace('-', '_');
-        String json = "{\"description\":null,\"name\":\"" + testConnectorName + "\"," +
+        String resultConnectorName =  "test_" + UUID.randomUUID().toString().replace('-', '_');
+        String json = "{\"description\":null,\"name\":\"" + resultConnectorName + "\"," +
                 "\"settings\":{\"api_key\":\"\",\"api_hostname\":\"\"}," +
                 "\"shared\":false,\"type\":\"treasure_data\"}";
 
@@ -148,23 +114,79 @@ public class TdResultExportIT
         JsonNode jsonNode = objectMapper.readTree(response.body().string());
         connectionId = jsonNode.get("id").asText();
 
-        String resultConnectionSettings = "{\"user_database_name\":\"" + this.database + "\"," +
-                "\"user_table_name\":\"" + this.table + "\"}";
+        List<FullHttpRequest> requests = Collections.synchronizedList(new ArrayList<>());
 
-        TDExportResultJobRequest req = TDExportResultJobRequest.builder()
-                .jobId(this.jobId)
-                .resultConnectionId(connectionId)
-                .resultConnectionSettings(resultConnectionSettings)
+        addWorkflow(projectDir, "acceptance/td/td_result_export/td_result_export.dig");
+
+        proxyServer = startRequestTrackingProxy(requests);
+
+        String proxyUrl = "http://" + proxyServer.getListenAddress().getHostString() + ":" + proxyServer.getListenAddress().getPort();
+
+        TemporaryDigdagServer server = TemporaryDigdagServer.builder()
+                .configuration(Secrets.secretsServerConfiguration())
+                .environment(ImmutableMap.of("http_proxy", proxyUrl))
                 .build();
-        client.submitResultExportJob(req);
+
+        server.start();
+
+        copyResource("acceptance/td/td_result_export/td_result_export.dig", projectDir.resolve("td_result_export.dig"));
+        TestUtils.addWorkflow(projectDir, "acceptance/td/td_result_export/td_result_export.dig");
+
+        Id projectId = TestUtils.pushProject(server.endpoint(), projectDir);
+
+        DigdagClient digdagClient = DigdagClient.builder()
+                .host(server.host())
+                .port(server.port())
+                .build();
+
+        digdagClient.setProjectSecret(projectId, "td.apikey", TD_API_KEY);
+
+        Id attemptId = pushAndStart(server.endpoint(), projectDir, "td_result_export", ImmutableMap.of(
+                "job_id", sampleJobId,
+                "result_settings", "{\"user_database_name\":\""+database+"\",\"user_table_name\":\""+table+"\",\"mode\":\"replace\"}",
+                "result_connection", resultConnectorName,
+                "td.use_ssl", "false")
+        );
+
+        expect(Duration.ofMinutes(5), attemptSuccess(server.endpoint(), attemptId));
+
+        String selectCountJobId = client.submit(TDJobRequest.newPrestoQuery(database, "select count(*) from " + table));
+        TestUtils.expect(Duration.ofMinutes(5), jobSuccess(client, selectCountJobId));
+
+        List<ArrayNode> result = downloadResult(selectCountJobId);
+        assertThat(result.get(0).get(0).asInt(), is(5000));
     }
 
-    @After
-    public void deleteQuery()
+    private List<ArrayNode> downloadResult(String jobId)
     {
-        if (client != null) {
-            client.deleteSavedQuery(savedQuery);
-        }
+        return client.jobResult(jobId, TDResultFormat.JSON, input -> {
+            try {
+                List<String> lines = CharStreams.readLines(new InputStreamReader(input));
+                ObjectReader reader = objectMapper().readerFor(ArrayNode.class);
+                List<ArrayNode> result = new ArrayList<>();
+                for (String line : lines) {
+                    result.add(reader.readValue(line));
+                }
+                return result;
+            }
+            catch (IOException e) {
+                throw Throwables.propagate(e);
+            }
+        });
+    }
+
+    private static Callable<Boolean> jobSuccess(TDClient client, String jobId)
+    {
+        return () -> {
+            TDJobSummary status = client.jobStatus(jobId);
+            if (status.getStatus() == TDJob.Status.SUCCESS) {
+                return true;
+            }
+            if (status.getStatus().isFinished()) {
+                fail(status.getStatus().toString());
+            }
+            return false;
+        };
     }
 
     @After
@@ -179,30 +201,5 @@ public class TdResultExportIT
                     .build();
             httpClient.newCall(request).execute();
         }
-    }
-
-    @After
-    public void deleteDatabase()
-    {
-        if (client != null && database != null) {
-            client.deleteDatabase(database);
-        }
-    }
-
-    private TDSavedQuery createQuery(String name)
-    {
-        return saveQuery(TDSavedQuery.newBuilder(
-                name,
-                TDJob.Type.PRESTO,
-                database,
-                "select 1",
-                "Asia/Tokyo")
-                .build());
-    }
-
-    private TDSavedQuery saveQuery(TDSaveQueryRequest request)
-    {
-        this.savedQuery = request.getName();
-        return client.saveQuery(request);
     }
 }
