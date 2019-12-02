993c5f0567ded3118c0474743b322b5811ce311f
TrsNium <trsnium@gmail.com>
2019-12-02 18:20:18 +0900

    support affinity, tolerations and volumes

--- digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClient.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClient.java
@@ -3,9 +3,12 @@ package io.digdag.standards.command.kubernetes;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.CharStreams;
+import io.digdag.core.storage.StorageManager;
 import io.digdag.client.config.Config;
+import io.digdag.client.config.ConfigException;
 import io.digdag.spi.CommandContext;
 import io.digdag.spi.CommandRequest;
+import io.digdag.spi.TaskRequest;
 import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.ContainerBuilder;
 import io.fabric8.kubernetes.api.model.ContainerStatus;
@@ -13,15 +16,25 @@ import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.EnvVarBuilder;
 import io.fabric8.kubernetes.api.model.PodSpec;
 import io.fabric8.kubernetes.api.model.PodSpecBuilder;
+import io.fabric8.kubernetes.api.model.Affinity;
+import io.fabric8.kubernetes.api.model.Toleration;
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeMount;
 import io.fabric8.kubernetes.api.model.Quantity;
 import io.fabric8.kubernetes.api.model.ResourceRequirements;
 import io.fabric8.kubernetes.client.dsl.PodResource;
+import io.fabric8.kubernetes.client.utils.Serialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.google.common.annotations.VisibleForTesting;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class DefaultKubernetesClient
@@ -51,15 +64,16 @@ public class DefaultKubernetesClient
     {
         final Container container = createContainer(context, request, name, commands, arguments);
         final PodSpec podSpec = createPodSpec(context, request, container);
-        final io.fabric8.kubernetes.api.model.Pod pod = client.pods()
-                .inNamespace(client.getNamespace())
+        io.fabric8.kubernetes.api.model.Pod pod = client.pods()
                 .createNew()
                 .withNewMetadata()
                 .withName(name)
+                .withNamespace(client.getNamespace())
                 .withLabels(getPodLabels())
                 .endMetadata()
                 .withSpec(podSpec)
                 .done();
+
         return Pod.of(pod);
     }
 
@@ -120,26 +134,43 @@ public class DefaultKubernetesClient
         return ImmutableMap.of();
     }
 
-    protected Container createContainer(final CommandContext context, final CommandRequest request,
+    @VisibleForTesting
+    Container createContainer(final CommandContext context, final CommandRequest request,
             final String name, final List<String> commands, final List<String> arguments)
     {
-        return new ContainerBuilder()
+        final TaskRequest taskRequest = context.getTaskRequest();
+        final Config taskConfig = taskRequest.getConfig();
+        ContainerBuilder containerBuilder = new ContainerBuilder()
                 .withName(name)
                 .withImage(getContainerImage(context, request))
                 .withEnv(toEnvVars(getEnvironments(context, request)))
                 .withResources(toResourceRequirements(getResourceLimits(context, request), getResourceRequests(context, request)))
                 .withCommand(commands)
-                .withArgs(arguments)
-                .build();
+                .withArgs(arguments);
+
+
+      final JsonNode node = taskConfig.getInternalObjectNode();
+      if (node.has("kubernetes")) {
+          final JsonNode kubernetesNode = node.get("kubernetes");
+          if (kubernetesNode.has("container")) {
+              final JsonNode containerNode = kubernetesNode.get("container");
+              if (containerNode.has("volumeMounts")) {
+                  containerBuilder.withVolumeMounts(convertToResourceList(containerNode.get("volumeMounts"), VolumeMount.class));
+              }
+          }
+      }
+        return containerBuilder.build();
     }
 
-    protected PodSpec createPodSpec(final CommandContext context, final CommandRequest request,
+    @VisibleForTesting
+    PodSpec createPodSpec(final CommandContext context, final CommandRequest request,
             final Container container)
     {
         // TODO
         // Revisit what values should be extracted as config params or system config params
-
-        return new PodSpecBuilder()
+        final TaskRequest taskRequest = context.getTaskRequest();
+        final Config taskConfig = taskRequest.getConfig();
+        PodSpecBuilder podSpecBuilder =  new PodSpecBuilder()
                 //.withHostNetwork(true);
                 //.withDnsPolicy("ClusterFirstWithHostNet");
                 .addToContainers(container)
@@ -147,8 +178,40 @@ public class DefaultKubernetesClient
                 // Restart policy is "Never" by default since it needs to avoid executing the operator multiple times. It might not
                 // make the script idempotent.
                 // https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#restart-policy
-                .withRestartPolicy("Never")
-                .build();
+                .withRestartPolicy("Never");
+
+      final JsonNode node = taskConfig.getInternalObjectNode();
+      if (node.has("kubernetes")) {
+          final JsonNode kubernetesNode = node.get("kubernetes");
+          if (kubernetesNode.has("pod")) {
+              final JsonNode podNode = kubernetesNode.get("pod");
+              if (podNode.has("affinity")) {
+                  podSpecBuilder.withAffinity(Serialization.unmarshal(podNode.get("affinity").toString(), Affinity.class));
+              }
+
+              if (podNode.has("tolerations")) {
+                  podSpecBuilder.withTolerations(convertToResourceList(podNode.get("tolerations"), Toleration.class));
+              }
+
+              if (podNode.has("volumes")) {
+                  podSpecBuilder.withVolumes(convertToResourceList(podNode.get("volumes"), Volume.class));
+              }
+          }
+      }
+      return podSpecBuilder.build();
+    }
+
+    protected <T> List<T> convertToResourceList(final JsonNode node, final Class<T> type)
+    {
+        List<T> resourcesList = new ArrayList<>();
+        if (node.isArray()){
+            for (JsonNode resource : node) {
+                resourcesList.add(Serialization.unmarshal(resource.toString(), type));
+            }
+        } else {
+            resourcesList.add(Serialization.unmarshal(node.toString(), type));
+        }
+        return resourcesList;
     }
 
     protected String getContainerImage(final CommandContext context, final CommandRequest request)
@@ -183,7 +246,7 @@ public class DefaultKubernetesClient
         return envVars.build();
     }
 
-    private static ResourceRequirements toResourceRequirements(
+    protected static ResourceRequirements toResourceRequirements(
             final Map<String, String> limits,
             final Map<String, String> requests)
     {
---
+++ digdag-standards/src/test/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClientTest.java
@@ -0,0 +1,146 @@
+package io.digdag.standards.command.kubernetes;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import io.digdag.spi.CommandContext;
+import io.digdag.spi.CommandRequest;
+import static io.digdag.client.config.ConfigUtils.newConfig;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static io.digdag.core.workflow.OperatorTestingUtils.newContext;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.ContainerBuilder;
+import io.fabric8.kubernetes.api.model.PodSpec;
+import io.fabric8.kubernetes.api.model.PodSpecBuilder;
+import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
+import io.fabric8.kubernetes.api.model.AffinityBuilder;
+import io.fabric8.kubernetes.api.model.NodeAffinityBuilder;
+import io.fabric8.kubernetes.api.model.NodeSelectorBuilder;
+import io.fabric8.kubernetes.api.model.NodeSelectorTermBuilder;
+import io.fabric8.kubernetes.api.model.NodeSelectorRequirementBuilder;
+import io.fabric8.kubernetes.api.model.Container;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import io.digdag.spi.TaskRequest;
+import io.digdag.client.config.Config;
+import static io.digdag.core.workflow.OperatorTestingUtils.newTaskRequest;
+import static io.digdag.core.workflow.OperatorTestingUtils.newContext;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DefaultKubernetesClientTest
+{
+
+    private KubernetesClientConfig kubernetesClientConfig;
+    private io.fabric8.kubernetes.client.DefaultKubernetesClient k8sDefaultKubernetesClient;
+    private CommandContext commandContext;
+    private CommandRequest commandRequest;
+
+    @Before
+    public void setUp()
+            throws Exception
+    {
+        kubernetesClientConfig = mock(KubernetesClientConfig.class);
+        k8sDefaultKubernetesClient =  mock(io.fabric8.kubernetes.client.DefaultKubernetesClient.class);
+        commandContext = mock(CommandContext.class);
+        commandRequest = mock(CommandRequest.class);
+    }
+
+    @Test
+    public void testCreateContainer()
+            throws Exception
+    {
+        final Config taskRequestConfig = newConfig()
+                .set("kubernetes", newConfig().set(
+                        "container", newConfig().set(
+                                "volumeMounts", ImmutableList.of(
+                                      newConfig().set("mountPath", "/test-ebs").set("name", "test")))))
+                .set("docker", newConfig().set("image", "test"));
+
+        final TaskRequest taskRequest = newTaskRequest().withConfig(taskRequestConfig);
+        when(commandContext.getTaskRequest()).thenReturn(taskRequest);
+        DefaultKubernetesClient defaultKubernetesClient = new DefaultKubernetesClient(kubernetesClientConfig, k8sDefaultKubernetesClient);
+
+        String podName = "test";
+        List<String> commands = new ArrayList<>();
+        List<String> arguments = new ArrayList<>();
+        Container container = defaultKubernetesClient.createContainer(commandContext, commandRequest, podName, commands, arguments);
+
+        Container desiredContainer = new ContainerBuilder()
+            .withName(podName)
+            .withImage("test")
+            .withCommand(commands)
+            .withArgs(arguments)
+            .withResources(defaultKubernetesClient.toResourceRequirements(defaultKubernetesClient.getResourceLimits(commandContext, commandRequest), defaultKubernetesClient.getResourceRequests(commandContext, commandRequest)))
+            .withVolumeMounts(Arrays.asList(new VolumeMountBuilder().withName("test").withMountPath("/test-ebs").build())).build();
+
+        assertThat(container, is(desiredContainer));
+    }
+
+    @Test
+    public void testCreatePodSPec()
+            throws Exception
+    {
+        final Config taskRequestConfig = newConfig()
+                .set("kubernetes", newConfig().set(
+                        "pod", newConfig().set(
+                                "affinity", newConfig().set(
+                                        "nodeAffinity", newConfig().set(
+                                                "requiredDuringSchedulingIgnoredDuringExecution", newConfig().set(
+                                                        "nodeSelectorTerms", ImmutableList.of(
+                                                                newConfig().set(
+                                                                        "matchExpressions", ImmutableList.of(
+                                                                                newConfig().set("key", "failure-domain.beta.kubernetes.io/zone")
+                                                                                .set("operator", "In")
+                                                                                .set("values", ImmutableList.of(
+                                                                                                "asia-northeast1-a"
+                                )))))))))));
+
+        final TaskRequest taskRequest = newTaskRequest().withConfig(taskRequestConfig);
+        when(commandContext.getTaskRequest()).thenReturn(taskRequest);
+        DefaultKubernetesClient defaultKubernetesClient = new DefaultKubernetesClient(kubernetesClientConfig, k8sDefaultKubernetesClient);
+
+        Container container = mock(Container.class);
+        PodSpec podSpec = defaultKubernetesClient.createPodSpec(commandContext, commandRequest, container);
+
+        PodSpec desiredPodSpec = new PodSpecBuilder()
+            .addToContainers(container)
+            .withRestartPolicy("Never")
+            .withAffinity(new AffinityBuilder()
+                .withNodeAffinity(new NodeAffinityBuilder()
+                  .withRequiredDuringSchedulingIgnoredDuringExecution(new NodeSelectorBuilder()
+                    .addToNodeSelectorTerms(0,
+                      new NodeSelectorTermBuilder().addToMatchExpressions(0,
+                        new NodeSelectorRequirementBuilder()
+                          .withKey("failure-domain.beta.kubernetes.io/zone")
+                          .addToValues(0,"asia-northeast1-a")
+                          .withOperator("In")
+                          .build()
+                        )
+                      .build()
+                      )
+                    .build()
+                    )
+                  .build()
+                  )
+                .build()
+                )
+            .build();
+
+        assertThat(podSpec, is(desiredPodSpec));
+    }
+}
