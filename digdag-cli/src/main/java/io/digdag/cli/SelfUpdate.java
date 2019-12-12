2d9f965891f645cd1d378dc63035ee9176065575
You Yamagata <youy@bg8.so-net.ne.jp>
2019-12-12 17:51:27 +0900

    Next snapshot 0.9.41

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -90,7 +90,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ " + programName + " selfupdate");
-        err.println("    $ " + programName + " selfupdate 0.9.40");
+        err.println("    $ " + programName + " selfupdate 0.9.41-SNAPSHOT");
         err.println("");
         return systemExit(error);
     }
