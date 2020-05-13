5df1f42707feb7de8f9e8013b7273a59a5e1a38c
Mitsunori Komatsu <komamitsu@gmail.com>
2020-05-13 14:55:03 +0900

    Avoid building all CI nodes with UI

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -9,6 +9,13 @@ if [[ $(( 1 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
     "${BASEDIR}/validate.sh"
 fi
 
+if [[ $(( 2 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
+  UI_BUILD_OPT=""
+else
+  # Don't need to build UI always
+  UI_BUILD_OPT="-PwithoutUi"
+fi
+
 "${BASEDIR}/run_test_h2.sh"
-"${BASEDIR}/run_test_pg.sh"
+"${BASEDIR}/run_test_pg.sh ${UI_BUILD_OPT}"
 
