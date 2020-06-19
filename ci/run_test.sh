0d81ed356a8ed62c85fcdbf40a1c9de959e86efa
Mitsunori Komatsu <komamitsu@gmail.com>
2020-06-19 21:06:34 +0900

    Use '-x buildUi' instead of '-PwithoutUi'

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -9,13 +9,16 @@ if [[ $(( 1 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
     "${BASEDIR}/validate.sh"
 fi
 
+WITH_BUILD_UI=""
+WITHOUT_BUILD_UI="-x buildUi"
+
+# Don't need to build UI always
 if [[ $(( 2 % ${CI_NODE_TOTAL} )) -eq ${CI_NODE_INDEX} ]]; then
-  UI_BUILD_OPT=""
+  BUILD_UI_OPT=${WITH_BUILD_UI}
 else
-  # Don't need to build UI always
-  UI_BUILD_OPT="-PwithoutUi"
+  BUILD_UI_OPT=${WITHOUT_BUILD_UI}
 fi
 
-"${BASEDIR}/run_test_h2.sh" -PwithoutUi
-"${BASEDIR}/run_test_pg.sh" ${UI_BUILD_OPT}
+"${BASEDIR}/run_test_h2.sh" ${WITHOUT_BUILD_UI}
+"${BASEDIR}/run_test_pg.sh" ${BUILD_UI_OPT}
 
