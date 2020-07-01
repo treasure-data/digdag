b2304c8cee735284b52f7672864ea1a9426ea4bf
yoyama <youy.bg8@gmail.com>
2020-07-01 16:59:42 +0900

    Merge pull request #1428 from yoyama/prepare-for-v0.9.43-SNAPSHOT
    Start 0.9.43-SNAPSHOT

--- digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
+++ digdag-cli/src/main/java/io/digdag/cli/SelfUpdate.java
@@ -90,7 +90,7 @@ public class SelfUpdate
         err.println("");
         err.println("  Examples:");
         err.println("    $ " + programName + " selfupdate");
-        err.println("    $ " + programName + " selfupdate 0.9.42");
+        err.println("    $ " + programName + " selfupdate 0.9.43-SNAPSHOT");
         err.println("");
         return systemExit(error);
     }
