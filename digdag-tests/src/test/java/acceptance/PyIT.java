9cbf7f3841491ec0c369ce701ffb27dbf70292df
You Yamagata <youy@bg8.so-net.ne.jp>
2020-11-09 20:34:27 +0900

    Add a test.

--- digdag-tests/src/test/java/acceptance/PyIT.java
+++ digdag-tests/src/test/java/acceptance/PyIT.java
@@ -3,6 +3,7 @@ package acceptance;
 import io.digdag.client.DigdagClient;
 import io.digdag.client.api.Id;
 import io.digdag.client.api.RestSessionAttempt;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,10 +13,15 @@ import utils.TemporaryDigdagServer;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Matchers.isNotNull;
 import static utils.TestUtils.copyResource;
 import static utils.TestUtils.getAttemptId;
 import static utils.TestUtils.getAttemptLogs;
@@ -203,5 +209,57 @@ public class PyIT
         assertThat(logs, containsString("my error message (MyError)"));
         assertThat(logs, containsString(", in run"));
         assertThat(logs, containsString("ERROR_MESSAGE_BEGIN Python command failed with code 1"));
+    }
+
+    @Test
+    public void verifyLogWithSyntaxError()
+            throws Exception
+    {
+        final Path tempdir = folder.getRoot().toPath().toAbsolutePath();
+        final Path projectDir = tempdir.resolve("py");
+        final Path scriptsDir = projectDir.resolve("scripts");
+
+        // Create new project
+        final CommandStatus initStatus = main("init",
+                "-c", config.toString(),
+                projectDir.toString());
+        assertThat(initStatus.code(), is(0));
+        Files.createDirectories(scriptsDir);
+        copyResource("acceptance/py/syntax_error_python.dig", projectDir.resolve("syntax_error_python.dig"));
+        copyResource("acceptance/py/scripts/syntax_error.py", scriptsDir.resolve("syntax_error.py"));
+        copyResource("acceptance/py/scripts/__init__.py", scriptsDir.resolve("__init__.py"));
+
+        // Push the project
+        final CommandStatus pushStatus = main("push",
+                "--project", projectDir.toString(),
+                "py",
+                "-c", config.toString(),
+                "-e", server.endpoint(),
+                "-r", "4712");
+        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
+
+        // Start the workflow
+        final CommandStatus startStatus = main("start",
+                "-c", config.toString(),
+                "-e", server.endpoint(),
+                "py", "syntax_error_python",
+                "--session", "now");
+        assertThat(startStatus.code(), is(0));
+        final Id attemptId = getAttemptId(startStatus);
+
+        // Wait for the attempt to complete
+        RestSessionAttempt attempt = null;
+        for (int i = 0; i < 60; i++) {
+            attempt = client.getSessionAttempt(attemptId);
+            if (attempt.getDone()) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        final String logs = getAttemptLogs(client, attemptId);
+        assertTrue(logs != null);
+        final String regex = "\\[ERROR\\] [^\\n]*Task failed with unexpected error: Python command failed with code 1:[^\\n]*\\(SyntaxError\\)";
+        assertTrue(Pattern.compile(regex).matcher(logs).find());
     }
 }
