82717134268a2a4c4bd1b05a439df68cadb20e6d
Mitsunori Komatsu <komamitsu@gmail.com>
2020-11-10 11:51:09 +0900

    Avoid adding duplicated code

--- digdag-core/src/main/java/io/digdag/core/agent/InProcessTaskServerApi.java
+++ digdag-core/src/main/java/io/digdag/core/agent/InProcessTaskServerApi.java
@@ -19,12 +19,9 @@ public class InProcessTaskServerApi
     implements TaskServerApi
 {
     private static final Logger logger = LoggerFactory.getLogger(WorkflowExecutor.class);
-    private final ToLongFunction<TaskQueueLock> CONV_FUNC_FROM_TASK_QUEUE_LOCK_TO_INT = tql -> {
+    private final ToLongFunction<TaskQueueLock> CONV_FUNC_FROM_TASK_QUEUE_LOCK_TO_INT = taskQueueLock -> {
         try {
-            // Retried task's unique name has suffix `.r${retryCount}`.
-            // See io.digdag.core.workflow.WorkflowExecutor.encodeUniqueQueuedTaskName
-            String[] decodedPartsOfUniqueName = tql.getUniqueName().split("\\.");
-            return Long.parseLong(decodedPartsOfUniqueName[0]);
+            return WorkflowExecutor.parseTaskIdFromEncodedQueuedTaskName(taskQueueLock.getUniqueName());
         }
         catch (Throwable e) {
             logger.warn("Failed to convert TaskQueueLock.uniqueName to integer. The `uniqueName` will be handled as 0", e);
