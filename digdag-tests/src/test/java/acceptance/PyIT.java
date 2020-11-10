6dc36a517b7ef65465f4e7c087a1ccb381ee7a72
You Yamagata <youy@bg8.so-net.ne.jp>
2020-11-10 11:10:18 +0900

    Add a test and refactoring in PyIT.

--- digdag-tests/src/test/java/acceptance/PyIT.java
+++ digdag-tests/src/test/java/acceptance/PyIT.java
@@ -18,6 +18,7 @@ import java.util.regex.Pattern;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.contains;
@@ -68,41 +69,11 @@ public class PyIT
         copyResource("acceptance/echo_params/scripts/__init__.py", scriptsDir.resolve("__init__.py"));
         copyResource("acceptance/echo_params/scripts/echo_params.py", scriptsDir.resolve("echo_params.py"));
 
-        // Push the project
-        CommandStatus pushStatus = main("push",
-                "--project", projectDir.toString(),
-                "py",
-                "-c", config.toString(),
-                "-e", server.endpoint(),
-                "-r", "4711");
-        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
+        RestSessionAttempt attempt = pushAndStart(projectDir, "echo_params", "4709");
+        assertThat(attempt, notNullValue());
+        assertThat(attempt.getSuccess(), is(true));
 
-        // Start the workflow
-        Id attemptId;
-        {
-            CommandStatus startStatus = main("start",
-                    "-c", config.toString(),
-                    "-e", server.endpoint(),
-                    "py", "echo_params",
-                    "--session", "now");
-            assertThat(startStatus.code(), is(0));
-            attemptId = getAttemptId(startStatus);
-        }
-
-        // Wait for the attempt to complete
-        {
-            RestSessionAttempt attempt = null;
-            for (int i = 0; i < 30; i++) {
-                attempt = client.getSessionAttempt(attemptId);
-                if (attempt.getDone()) {
-                    break;
-                }
-                Thread.sleep(1000);
-            }
-            assertThat(attempt.getSuccess(), is(true));
-        }
-
-        String logs = getAttemptLogs(client, attemptId);
+        String logs = getAttemptLogs(client, attempt.getId());
         assertThat(logs, containsString("digdag params"));
     }
 
@@ -124,36 +95,11 @@ public class PyIT
         copyResource("acceptance/echo_params/scripts/__init__.py", scriptsDir.resolve("__init__.py"));
         copyResource("acceptance/echo_params/scripts/echo_params.py", scriptsDir.resolve("echo_params.py"));
 
-        // Push the project
-        final CommandStatus pushStatus = main("push",
-                "--project", projectDir.toString(),
-                "py",
-                "-c", config.toString(),
-                "-e", server.endpoint(),
-                "-r", "4711");
-        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
-
-        // Start the workflow
-        final CommandStatus startStatus = main("start",
-                "-c", config.toString(),
-                "-e", server.endpoint(),
-                "py", "config_python",
-                "--session", "now");
-        assertThat(startStatus.code(), is(0));
-        final Id attemptId = getAttemptId(startStatus);
-
-        // Wait for the attempt to complete
-        RestSessionAttempt attempt = null;
-        for (int i = 0; i < 30; i++) {
-            attempt = client.getSessionAttempt(attemptId);
-            if (attempt.getDone()) {
-                break;
-            }
-            Thread.sleep(1000);
-        }
+        RestSessionAttempt attempt = pushAndStart(projectDir, "config_python", "4710");
+        assertThat(attempt, notNullValue());
         assertThat(attempt.getSuccess(), is(true));
 
-        final String logs = getAttemptLogs(client, attemptId);
+        final String logs = getAttemptLogs(client, attempt.getId());
         assertThat(logs, containsString("python python"));
         assertThat(logs, containsString("python [u'python', u'-v']"));
     }
@@ -176,37 +122,12 @@ public class PyIT
         copyResource("acceptance/py/scripts/stacktrace.py", scriptsDir.resolve("stacktrace.py"));
         copyResource("acceptance/py/scripts/__init__.py", scriptsDir.resolve("__init__.py"));
 
-        // Push the project
-        final CommandStatus pushStatus = main("push",
-                "--project", projectDir.toString(),
-                "py",
-                "-c", config.toString(),
-                "-e", server.endpoint(),
-                "-r", "4711");
-        assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
+        RestSessionAttempt attempt = pushAndStart(projectDir, "stacktrace_python", "4711");
+        assertThat(attempt, notNullValue());
+        assertThat(attempt.getSuccess(), is(false));
 
-        // Start the workflow
-        final CommandStatus startStatus = main("start",
-                "-c", config.toString(),
-                "-e", server.endpoint(),
-                "py", "stacktrace_python",
-                "--session", "now");
-        assertThat(startStatus.code(), is(0));
-        final Id attemptId = getAttemptId(startStatus);
-
-        // Wait for the attempt to complete
-        RestSessionAttempt attempt = null;
-        for (int i = 0; i < 30; i++) {
-            attempt = client.getSessionAttempt(attemptId);
-            if (attempt.getDone()) {
-                break;
-            }
-            Thread.sleep(1000);
-        }
-
-        final String logs = getAttemptLogs(client, attemptId);
-        assertThat(logs, containsString("Task failed with unexpected error: Python command failed with code 1"));
-        assertThat(logs, containsString("my error message (MyError)"));
+        final String logs = getAttemptLogs(client, attempt.getId());
+        assertThat(logs, containsString("Task failed with unexpected error: Python command failed with code 1: my error message (MyError)"));
         assertThat(logs, containsString(", in run"));
         assertThat(logs, containsString("ERROR_MESSAGE_BEGIN Python command failed with code 1"));
     }
@@ -229,20 +150,60 @@ public class PyIT
         copyResource("acceptance/py/scripts/syntax_error.py", scriptsDir.resolve("syntax_error.py"));
         copyResource("acceptance/py/scripts/__init__.py", scriptsDir.resolve("__init__.py"));
 
+        RestSessionAttempt attempt = pushAndStart(projectDir, "syntax_error_python", "4712");
+        assertThat(attempt, notNullValue());
+        assertThat(attempt.getSuccess(), is(false));
+
+        final String logs = getAttemptLogs(client, attempt.getId());
+        assertTrue(logs != null);
+        assertThat(attempt.getSuccess(), is(false));
+        final String regex = "\\[ERROR\\] [^\\n]*Task failed with unexpected error: Python command failed with code 1:[^\\n]*\\(SyntaxError\\)";
+        assertTrue(Pattern.compile(regex).matcher(logs).find());
+    }
+
+    @Test
+    public void verifyLogWithDuplicateModuleError()
+            throws Exception
+    {
+        final Path tempdir = folder.getRoot().toPath().toAbsolutePath();
+        final Path projectDir = tempdir.resolve("py");
+        final Path scriptsDir = projectDir.resolve("scripts");
+
+        // Create new project
+        final CommandStatus initStatus = main("init",
+                "-c", config.toString(),
+                projectDir.toString());
+        assertThat(initStatus.code(), is(0));
+        Files.createDirectories(scriptsDir);
+        copyResource("acceptance/py/dup_module.dig", projectDir.resolve("dup_module.dig"));
+        copyResource("acceptance/py/scripts/tokenize.py", projectDir.resolve("tokenize.py"));
+
+        RestSessionAttempt attempt = pushAndStart(projectDir, "dup_module", "4713");
+        assertThat(attempt, notNullValue());
+        final String logs = getAttemptLogs(client, attempt.getId());
+        assertTrue(logs != null);
+        assertThat(attempt.getSuccess(), is(false));
+        final String regex = "\\[ERROR\\] [^\\n]*Task failed with unexpected error: Python command failed with code 1:[^\\n]*duplicae module name with standard library";
+        assertTrue(Pattern.compile(regex).matcher(logs).find());
+    }
+
+    private RestSessionAttempt pushAndStart(Path projectDir, String workflowName, String revision)
+            throws Exception
+    {
         // Push the project
         final CommandStatus pushStatus = main("push",
                 "--project", projectDir.toString(),
                 "py",
                 "-c", config.toString(),
                 "-e", server.endpoint(),
-                "-r", "4712");
+                "-r", revision);
         assertThat(pushStatus.errUtf8(), pushStatus.code(), is(0));
 
         // Start the workflow
         final CommandStatus startStatus = main("start",
                 "-c", config.toString(),
                 "-e", server.endpoint(),
-                "py", "syntax_error_python",
+                "py", workflowName,
                 "--session", "now");
         assertThat(startStatus.code(), is(0));
         final Id attemptId = getAttemptId(startStatus);
@@ -256,10 +217,6 @@ public class PyIT
             }
             Thread.sleep(1000);
         }
-
-        final String logs = getAttemptLogs(client, attemptId);
-        assertTrue(logs != null);
-        final String regex = "\\[ERROR\\] [^\\n]*Task failed with unexpected error: Python command failed with code 1:[^\\n]*\\(SyntaxError\\)";
-        assertTrue(Pattern.compile(regex).matcher(logs).find());
+        return attempt;
     }
 }
