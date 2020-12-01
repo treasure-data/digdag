2bc2558cb04868d1a59fbf62d3c91b60f5ab59a5
Shota Suzuki <aqr.aqua@gmail.com>
2020-12-01 18:53:18 +0900

    Add support for specifying KubernetesCommandExecutor

--- digdag-standards/src/main/java/io/digdag/standards/command/CommandExecutorModule.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/CommandExecutorModule.java
@@ -8,6 +8,8 @@ import io.digdag.spi.CommandExecutor;
 import io.digdag.spi.CommandExecutorFactory;
 import io.digdag.standards.command.ecs.DefaultEcsClientFactory;
 import io.digdag.standards.command.ecs.EcsClientFactory;
+import io.digdag.standards.command.kubernetes.DefaultKubernetesClientFactory;
+import io.digdag.standards.command.kubernetes.KubernetesClientFactory;
 
 public class CommandExecutorModule
     implements Module
@@ -16,13 +18,14 @@ public class CommandExecutorModule
     public void configure(Binder binder)
     {
         binder.bind(EcsClientFactory.class).to(DefaultEcsClientFactory.class).in(Scopes.SINGLETON);
-        //binder.bind(KubernetesClientFactory.class).to(DefaultKubernetesClientFactory.class).in(Scopes.SINGLETON);
+        binder.bind(KubernetesClientFactory.class).to(DefaultKubernetesClientFactory.class).in(Scopes.SINGLETON);
 
         // CommandExecutor
         Multibinder<CommandExecutorFactory> commandExecutorBinder = Multibinder.newSetBinder(binder, CommandExecutorFactory.class);
         commandExecutorBinder.addBinding().to(SimpleCommandExecutorFactory.class).in(Scopes.SINGLETON);
         commandExecutorBinder.addBinding().to(DockerCommandExecutorFactory.class).in(Scopes.SINGLETON);
         commandExecutorBinder.addBinding().to(EcsCommandExecutorFactory.class).in(Scopes.SINGLETON);
+        commandExecutorBinder.addBinding().to(KubernetesCommandExecutorFactory.class).in(Scopes.SINGLETON);
 
         binder.bind(CommandExecutor.class).toProvider(CommandExecutorProvider.class).in(Scopes.SINGLETON);
     }
