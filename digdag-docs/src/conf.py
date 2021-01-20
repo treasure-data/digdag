070091dd4e1aa605a970f561d40171d0590aab62
You Yamagata <youy@bg8.so-net.ne.jp>
2021-01-20 17:17:51 +0900

    Fix table in markdown related issue.

--- digdag-docs/src/conf.py
+++ digdag-docs/src/conf.py
@@ -34,7 +34,7 @@ release = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags'])[1
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark', 'sphinx_markdown_tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
