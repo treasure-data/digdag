97155202842b6cdd4a8d132c62aca38ab44bde8a
Naoto Yokoyama <builtinnya@gmail.com>
2021-07-28 16:44:07 +0900

    Fix polling (ECS) command executors fail to run tasks on retry
    Signed-off-by: Naoto Yokoyama <builtinnya@gmail.com>

--- digdag-plugin-utils/src/test/java/io/digdag/util/BaseOperatorTest.java
+++ digdag-plugin-utils/src/test/java/io/digdag/util/BaseOperatorTest.java
@@ -1,6 +1,8 @@
 package io.digdag.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import io.digdag.client.config.Config;
 import io.digdag.client.config.ConfigElement;
 import io.digdag.client.config.ConfigFactory;
@@ -99,6 +101,37 @@ public class BaseOperatorTest
             assertThat(e, is(ex));
             assertThat(e.isError(), is(false));
             assertThat(e.getRetryInterval().get(), is(INTERVAL));
+        }
+    }
+
+    @Test
+    public void verifyNonPollingRuntimeExceptionRemovesCommandStatusOnRetry()
+            throws Exception
+    {
+        config.set("_retry", 10);
+
+        ObjectNode commandStatus = new ObjectMapper().createObjectNode().put("foo", "bar");
+        state.set("commandStatus", commandStatus);
+
+        RuntimeException ex = new RuntimeException("Command failed");
+
+        BaseOperator op = new BaseOperator(newContext(temporaryFolder.getRoot().toPath(), request))
+        {
+            @Override
+            public TaskResult runTask()
+            {
+                throw ex;
+            }
+        };
+
+        try {
+            op.run();
+            fail();
+        }
+        catch (TaskExecutionException e) {
+            assertThat(e.isError(), is(true));
+            assertThat(e.getCause(), is(ex));
+            assertThat(e.getStateParams(configFactory).get().has("commandStatus"), is(false));
         }
     }
 
