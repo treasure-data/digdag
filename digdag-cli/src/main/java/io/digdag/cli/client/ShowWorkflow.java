75bdb198e192b09a4f9316bba76a4b199047d869
o-mura <yuu.ohmura@treasure-data.com>
2021-10-22 15:06:20 +0900

    enable count and last-id option

--- digdag-cli/src/main/java/io/digdag/cli/client/ShowWorkflow.java
+++ digdag-cli/src/main/java/io/digdag/cli/client/ShowWorkflow.java
@@ -1,7 +1,10 @@
 package io.digdag.cli.client;
 
+import com.beust.jcommander.Parameter;
+import com.google.common.base.Optional;
 import io.digdag.cli.SystemExitException;
 import io.digdag.client.DigdagClient;
+import io.digdag.client.api.Id;
 import io.digdag.client.api.RestProject;
 import io.digdag.client.api.RestWorkflowDefinition;
 
@@ -14,6 +17,12 @@ import static io.digdag.cli.SystemExitException.systemExit;
 public class ShowWorkflow
     extends ClientCommand
 {
+    @Parameter(names = {"--last-id"})
+    Id lastId = null;
+	
+    @Parameter(names = {"--count"})
+    int count = 100;
+
     @Override
     public void mainWithClientException()
         throws Exception
@@ -36,6 +45,9 @@ public class ShowWorkflow
     public SystemExitException usage(String error)
     {
         err.println("Usage: " + programName + " workflows [project-name] [name]");
+	err.println("  Options:");
+	err.println("  --count number   number of workflows");
+	err.println("  --lastid id      last id of workflow");
         showCommonOptions();
         return systemExit(error);
     }
@@ -53,7 +65,8 @@ public class ShowWorkflow
             }
         }
         else {
-            List<RestWorkflowDefinition> defs = client.getWorkflowDefinitions().getWorkflows();
+            //List<RestWorkflowDefinition> defs = client.getWorkflowDefinitions().getWorkflows();
+	    List<RestWorkflowDefinition> defs = client.getWorkflowDefinitions(Optional.fromNullable(lastId), count).getWorkflows();
             String lastProjName = null;
             for (RestWorkflowDefinition def : defs) {
                 if (!def.getProject().getName().equals(lastProjName)) {
