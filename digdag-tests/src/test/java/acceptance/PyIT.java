4a42e15293c2b49e131da8e10c814fe7658764c3
Aki Ariga <ariga@treasure-data.com>
2022-03-26 09:14:02 -0700

    Fix typo

--- digdag-tests/src/test/java/acceptance/PyIT.java
+++ digdag-tests/src/test/java/acceptance/PyIT.java
@@ -183,7 +183,7 @@ public class PyIT
         final String logs = getAttemptLogs(client, attempt.getId());
         assertTrue(logs != null);
         assertThat(attempt.getSuccess(), is(false));
-        final String regex = "\\[ERROR\\] [^\\n]*Task failed with unexpected error: Python command failed with code 1.*Error messages from python:[^\\n]*duplicae module name with standard library";
+        final String regex = "\\[ERROR\\] [^\\n]*Task failed with unexpected error: Python command failed with code 1.*Error messages from python:[^\\n]*duplicate module name with standard library";
         assertTrue(Pattern.compile(regex, Pattern.DOTALL).matcher(logs).find());
     }
 
